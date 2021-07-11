05651a19ba5aa82e9e0540422e227af7817114b1
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-07-11 14:03:04 +0200

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,9 +1,32 @@
-class Person {
-  sayHello() {
-    console.log('Hello!');
-  }
-};
+// class Person {
+//   constructor(name) {
+//     this.name = name;
+//   }
+//
+//   sayHello() {
+//     console.log('Hello!');
+//   }
+// };
+//
+// const alice = new Person();
+// alice.sayHello();
+// console.log(typeof Person);
 
-const alice = new Person();
-alice.sayHello();
-console.log(typeof Person);
+//------------------------------------------------------------------------------------------
+
+class Country {
+  constructor(currency, language, currencyRatioToUsd) {
+    this.currency = currency;
+    this.language = language;
+    this.currencyRatioToUsd = currencyRatioToUsd;
+  }
+  convertMoney(valueInLocalCurrency) {
+    return valueInLocalCurrency / this.currencyRatioToUsd;
+  }
+}
+
+const poland = new Country('PLN', 'Polish', 4);
+const souvenirInPln = 50;
+const souvenirInUsd = poland.convertMoney(souvenirInPln);
+
+console.log(souvenirInUsd);
