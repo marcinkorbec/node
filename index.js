b5b33e38ae1945cc0c08b6885056e0516cf36e9e
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-07-11 13:24:30 +0200

    create index2.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const a = 2;
+const b = 3;
+
+console.log ('Hello, World!', a + b + c);
+
+
+// @TODO Stworzyć kalkulator!
---
+++ index2.js
@@ -0,0 +1,9 @@
+class Person {
+  sayHello() {
+    console.log('Hello!');
+  }
+};
+
+const alice = new Person();
+alice.sayHello();
+console.log(typeof Person);
