71c35371318a9db3aa8bcc0e83b0657ed993a323
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-07-14 16:21:37 +0200

    update

--- index5.js
+++ index5.js
@@ -1,10 +1,20 @@
 const grades = [3, 4, 5, 6, 3, 4, 2, 5, 6];
 
-const totalSum = grades
-  .reduce(function(a, b) { return a + b; }, 0);
+const totalSum = grades.reduce(function(a, b) { return a + b; }, 0);
+const average = totalSum/grades.length;
 
 console.log('Sum is: ' + totalSum);
-
-const average = totalSum/grades.length
-
 console.log('Average is: ' + average);
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+
+const cities = ['Wrocław', 'Poznań', 'Kraków', 'Warszawa', 'Katowice', 'Bytom', 'Jelenia Góra', 'Jastrzębie-Zdrój', 'Rabka-Zdrój'];
+
+cities
+  .filter((el => el.length % 2 === 0))
+  .map(el => el.toUpperCase())
+  .forEach(el => console.log(el));
+
+
+
+
