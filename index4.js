7ac8a799b6e14d3eadab70cc363cf14d538018fc
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-07-14 13:34:33 +0200

    Update index4.js

--- index4.js
+++ index4.js
@@ -11,25 +11,32 @@
 //
 // ar.forEach(num => console.log(num));
 
-const array = [1,2,3];
+// const array = [1,2,3];
 
-array.forEach((element, index) => {
-  console.log (element);
-});
+// array.forEach((element, index) => {
+//   console.log (element);
+// });
+//
+// //to samo można uzyskać za pomocą zwykłej pętli for:
+//
+// for (let i = 0; i<array.length; i++) {
+//   const element = array[i];
+//   console.log(element);
+// };
+//
+// array.forEach((element, index, array) => {
+//   array[index] = element + 2;
+// });
 
-//to samo można uzyskać za pomocą zwykłej pętli for:
-
-for (let i = 0; i<array.length; i++) {
-  const element = array[i];
-  console.log(element);
-};
-
-array.forEach((element, index, array) => {
-  array[index] = element + 2;
-});
-
-console.log (array);
+// console.log (array);
 
 //----------------------------------------------------------------
 
 const numbers = [1, 2, 3];
+
+const newNumbers = numbers.map((number) => {
+ return number + 2;
+});
+
+console.log(newNumbers);
+console.log(numbers);
