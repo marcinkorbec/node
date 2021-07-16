02f61f2102f8ac0bc07038bddeb21d1b59bac427
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-07-16 15:15:07 +0200

    summary update

--- T3D5/T3D5.js
+++ T3D5/T3D5.js
@@ -1,35 +1,50 @@
+// const btn = document.querySelector('.btn-toggle-color');
+// const body = document.body;
+// let click = 0;
+//
+// btn.addEventListener('click', () => {
+//   const isDarkMode = body.classList.toggle('dark-mode');
+//
+//   if (isDarkMode) {
+//     btn.innerText = 'Turn on light mode';
+//   } else {
+//     btn.innerText = 'Turn on dark mode';
+//   }
+//   ++click;
+//   document.querySelector('h1').innerText = click;
+// });
+//
+//
+// localStorage.setItem('courseName', 'Mega Kurs');
+// localStorage.getItem('courseName');
+//
+//
+// //Get last visit date
+// const lastSavedVisitDate = localStorage.getItem('last-visit');
+//
+// if ( lastSavedVisitDate === null) {
+//   console.log('You are first time here!');
+// } else {
+//   console.log('You were here on ' + lastSavedVisitDate)
+// }
+//
+// //Set last visit date
+// const date = new Date();
+// const dateString = date.toString();
+//
+// localStorage.setItem('last-visit', dateString);
+
+//----------------------------------------------------------------------------------------------
+//   ZADANIE
+//----------------------------------------------------------------------------------------------
+
 const btn = document.querySelector('.btn-toggle-color');
-const body = document.body;
-let click = 0;
+const h1 = document.querySelector('h1');
 
+let click = Number(localStorage.getItem('counter'));
+h1.innerText = click;
 btn.addEventListener('click', () => {
-  const isDarkMode = body.classList.toggle('dark-mode');
-
-  if (isDarkMode) {
-    btn.innerText = 'Turn on light mode';
-  } else {
-    btn.innerText = 'Turn on dark mode';
-  }
-  ++click;
-  document.querySelector('h1').innerText = click;
+  let counter = ++click;
+  h1.innerText = counter;
+  localStorage.setItem('counter', String(counter));
 });
-
-
-localStorage.setItem('courseName', 'Mega Kurs');
-localStorage.getItem('courseName');
-
-
-//Get last visit date
-const lastSavedVisitDate = localStorage.getItem('last-visit');
-
-if ( lastSavedVisitDate === null) {
-  console.log('You are first time here!');
-} else {
-  console.log('You were here on ' + lastSavedVisitDate)
-}
-
-//Set last visit date
-const date = new Date();
-const dateString = date.toString();
-
-localStorage.setItem('last-visit', dateString);
