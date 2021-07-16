c5e622327863e9fb746a4bafd688b1790f43c812
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-07-16 21:29:35 +0200

    summary

--- T3D5 zad1/app.js
+++ T3D5 zad1/app.js
@@ -1,39 +1,78 @@
-const btn1 = document.querySelector('.first-btn');
-const btn2 = document.querySelector('.second-btn');
+{
+  const btn1 = document.querySelector('.first-btn');
+  const btn2 = document.querySelector('.second-btn');
 
-const good = document.querySelector('.good');
-const bad = document.querySelector('.bad');
-const text = document.querySelector('.text');
+  const good = document.querySelector('.good');
+  const bad = document.querySelector('.bad');
+
 
 //------DOWNLOAD VARIABLE FROM LOCAL STORAGE------------------------------------------
 
-let click1 = Number(localStorage.getItem('counter-good'));
-good.innerText = click1;
+  let click1 = Number(localStorage.getItem('counter-good'));
+  good.innerText = click1;
 
-let click2 = Number(localStorage.getItem('counter-bad'));
-bad.innerText = click2;
+  let click2 = Number(localStorage.getItem('counter-bad'));
+  bad.innerText = click2;
+
+  if (click1 === null) {
+    good.innerText = 0;
+  } else if (click2 === null) {
+    bad.innerText = 0;
+  }
 
 //------SETTING THE LISTENING TO THE BUTTONS------------------------------------------
 
-btn1.addEventListener('click', () => {
-  let counterGood = ++click1;
-  good.innerText = counterGood;
-  localStorage.setItem('counter-good', String(counterGood));
-});
+  btn1.addEventListener('click', () => {
+    let counterGood = ++click1;
+    good.innerText = counterGood;
+    localStorage.setItem('counter-good', String(counterGood));
+  });
 
-btn2.addEventListener('click', () => {
-  let counterBad = ++click2;
-  bad.innerText = counterBad;
-  localStorage.setItem('counter-bad', String(counterBad));
-});
+  btn2.addEventListener('click', () => {
+    let counterBad = ++click2;
+    bad.innerText = counterBad;
+    localStorage.setItem('counter-bad', String(counterBad));
+  });
+}
+//-----------------------------------------------------------------------------------
+//        2. OBJECT IN LOCALSTORAGE VERSION
+//-----------------------------------------------------------------------------------
+{
+  const btn1 = document.querySelector('.btn-3');
+  const btn2 = document.querySelector('.btn-4');
 
-//-------SAVE DATA AS A OBJECT IN LOCAL STORAGE---------------------------------------
+  const good = document.querySelector('.moodGood');
+  const bad = document.querySelector('.moodBad');
+  const feelingInfo = localStorage.getItem('feelingInfo');
 
-let dataObj = {
-  moodGood: Number(localStorage.getItem('counter-good')),
-  moodBad: Number(localStorage.getItem('counter-bad')),
-};
+  //-----CHECK TYPE OF LOCALSTORAGE FEELING INFO and setting to localstorage---------
 
-localStorage.setItem('data-info', JSON.stringify(dataObj));
-let data = localStorage.getItem('data-info');
-text.innerText = data;
+  if (feelingInfo === null) {
+    let dataObj = {
+      moodGood: 0,
+      moodBad: 0,
+    };
+    good.innerText = dataObj.moodGood.toString();
+    bad.innerText = dataObj.moodBad.toString();
+    localStorage.setItem('feelingInfo', JSON.stringify(dataObj));
+  }
+
+  const parseDataObj = JSON.parse(localStorage.getItem('feelingInfo'));
+  good.innerText = parseDataObj.moodGood.toString();
+  bad.innerText = parseDataObj.moodBad.toString();
+
+  //------SETTING THE LISTENING TO THE BUTTONS--------------------------------------
+
+  btn1.addEventListener('click', () => {
+    parseDataObj.moodGood++;
+    good.innerText = parseDataObj.moodGood.toString();
+    localStorage.setItem('feelingInfo', JSON.stringify(parseDataObj));
+  })
+
+  btn2.addEventListener('click', () => {
+    parseDataObj.moodBad++;
+    bad.innerText = parseDataObj.moodBad.toString();
+    localStorage.setItem('feelingInfo', JSON.stringify(parseDataObj));
+  })
+
+}
