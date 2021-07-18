c51f7da9eedfd0701a6b1138b0c9b966b46e0ae9
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-07-18 14:28:24 +0200

    Update app.js

--- T3D5 dodatkowe/app.js
+++ T3D5 dodatkowe/app.js
@@ -1,9 +1,19 @@
 const getUsers = () => {
-  const url = 'https://randomuser.me/api/?results=50&gender=female&nat=es,de';
-  fetch(url)
-    .then(response => (console.log(response)))
-    .catch(err => console.log(err))
+  const url = 'https://randomuser.me/api/?results=500&nat=es,de';
 
+  fetch(url)//obietnica - oczekujący (pending)
+    //obietnica - rozstrzygnięta (spełnione | odrzucone)
+    //wykonuje się then - pozytywnie
+    .then(response => {
+      //(console.log(response));
+      if (response.status !== 200) {
+        throw Error('To nie jest odpowiedź 200!')
+      } else {
+        return response.json(); // fetch API = json() z body wyodrębnia json
+      }
+    })
+    .then(json => console.log(json))
+    .catch(err => console.log(err))
 }
 
 document.querySelector('button').addEventListener('click',getUsers);
