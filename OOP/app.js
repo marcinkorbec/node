ef6c16c1908412b9ea81ab7b644be8f896195ff7
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-07-20 14:17:36 +0200

    Update app.js

--- OOP/app.js
+++ OOP/app.js
@@ -2,8 +2,39 @@ class Basket {
   constructor() {
     this.items = [];
   }
+  add(item) {
+    this.items.push(item);
+  }
+  getTotalValue() {
+    return this.items.reduce((prev, curr) => prev + curr, 0);
+  }
+
+  //pętla
+//   getTotalValue() {
+//     let sum = 0;
+//     for (const product of this.items) {
+//       sum += product.price;
+//     }
+//   }
 }
 
+class Product {
+  constructor(productName, productPrice) {
+    this.name = productName;
+    this.price =productPrice;
+  }
+  setNewPrice(newPrice) {
+    this.price = newPrice;
+  }
+}
+
+
 const shopBasket = new Basket();
+
+const oranges = new Product('Pomarańcze LUZ', 7.55);
+const cucumbers = new Product('Ogórki LUZ',4.99);
+
+shopBasket.add(cucumbers);
+shopBasket.add(cucumbers);
 
 console.log(shopBasket);
