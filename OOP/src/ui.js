19bfaba26109b23699238ed83007b6d05b2c20d7
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-07-26 14:08:00 +0200

    Update ui.js

--- OOP/src/ui.js
+++ OOP/src/ui.js
@@ -2,7 +2,9 @@ const basket = new Basket();
 
 const buyBtns = [...document.querySelectorAll('[data-id]')];
 
-buyBtns[0].addEventListener('click', function(event) {
-  console.log(event.target.dataset.id);
-  console.log('Drugi sposob', this.getAttribute('data-id'));
-})
+for (const buyBtn of buyBtns) {
+  buyBtn.addEventListener('click', function(event) {
+    console.log(event.target.dataset.id);
+    console.log('Drugi sposob', this.getAttribute('data-id'));
+  })
+}
