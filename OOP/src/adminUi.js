96d79131850cfe7ad5ebf25a8563febd11b44eaa
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-07-27 19:11:14 +0200

    Update adminUi.js

--- OOP/src/adminUi.js
+++ OOP/src/adminUi.js
@@ -1,22 +1,23 @@
 const addProductForm = document.querySelector('.form-add-product');
 const nameInput = document.querySelector('[name="product-name"]');
 const priceInput = document.querySelector('[name="product-price"]');
-const productsList = document.querySelector('product-ol');
+const productsList = document.querySelector('.product-ol');
 
 function addProductToShop(event) {
   event.preventDefault();
-  console.log(nameInput);
-
   const name = nameInput.value;
   const price = Number(priceInput.value);
   const newLi = document.createElement('li');
   const newStrong = document.createElement('strong');
+  const newButton = document.createElement('button');
+  newButton.innerText = 'Kup Teraz!';
+  newButton.classList.add('btn-buy', 'btn', 'btn-primary');
   newStrong.innerText = name;
-  const newPriceText = document.createTextNode(` - ${price.toFixed(2)}`);
-
+  const newPriceText = document.createTextNode(` - ${price.toFixed(2)}zł `);
   newLi.appendChild(newStrong);
   newLi.appendChild(newPriceText);
-  productsList.appendChild(newLi)
+  newLi.appendChild(newButton);
+  productsList.appendChild(newLi);
 }
 
 addProductForm.addEventListener('submit', addProductToShop);
