d1da5b2eb6ae8f40e4495d72020bd582138e4d8a
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-07-29 13:19:21 +0200

    Update calculator.js

--- T5D2Z1/calculator.js
+++ T5D2Z1/calculator.js
@@ -2,21 +2,43 @@ class Calculator {
   constructor(a, b) {
     this.a = a;
     this.b = b;
+    if (Number.isNaN(a) || Number.isNaN(b)) {
+      try {
+        alert('Przynajmniej jedna z wartości nie jest liczbą!');
+      } finally {
+        alert('Koniec programu!');
+      }
+    } else if (a==='' || b==='') {
+      try {
+        alert('Nie wpisałeś liczby!')
+      } finally {
+        alert('Koniec programu!')
+      }
+    }
   }
 
-  add(a, b) {
-    this.items = a + b;
+  add() {
+    return this.items = this.a + this.b;
   }
 
   substract(a, b) {
-    this.items = a - b;
+     return this.items = this.a - this.b;
   }
 
   divide(a , b) {
-    this.items = a / b;
+    if (b === 0){
+      try{
+        alert('Próbujesz podzielić przez 0!');
+      } finally {
+        alert('Koniec programu!')
+      }
+    }
+    return this.items = this.a / this.b;
   }
 
   multiply(a, b) {
-    this.items = a * b;
+    return this.items = this.a * this.b;
   }
 }
+
+const a =
