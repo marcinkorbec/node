5e52ca3e641de38f8ad4871d8055e3d177344298
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-08-02 13:22:36 +0200

    Update index.js

--- T5D3/index.js
+++ T5D3/index.js
@@ -1,6 +1,12 @@
 class Warrior {
-
+  constructor(name, hitPoints, life) {
+    this.name = name;
+    this.hitPoints = hitPoints;
+    this.life = life;
+  }
 }
 
 
-console.log(new Warrior());
+const fighter1 = new Warrior('Baba Yaga', 9, 120);
+const fighter2 = new Warrior('Yanosik', 7, 140);
+console.log(fighter1, fighter2);
