4aabb2fad3b17fde96e9529b22ef3058548a4442
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-08-11 12:11:45 +0200

    update

--- stoper/main.js
+++ stoper/main.js
@@ -1,1 +1,16 @@
 // http://websamuraj.pl/examples/js/projekt11/
+
+const startStopBtn = document.querySelector('.main');
+const resetBtn = document.querySelector('.reset')
+
+function toggleText() {
+  if (startStopBtn.classList.contains('active')){
+    startStopBtn.classList.remove('active')
+    startStopBtn.textContent = 'Pauza';
+  } else {
+    startStopBtn.classList.add('active')
+    startStopBtn.textContent = 'Start';
+  }
+}
+
+startStopBtn.addEventListener('click', toggleText());
