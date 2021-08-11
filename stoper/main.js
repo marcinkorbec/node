90bd935456a999beb1213180d7615db719e200ee
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-08-11 12:40:20 +0200

    Update main.js

--- stoper/main.js
+++ stoper/main.js
@@ -2,15 +2,28 @@
 
 const startStopBtn = document.querySelector('.main');
 const resetBtn = document.querySelector('.reset')
+const panel = document.querySelector('.time div');
+let time = 0;
+let active = false;
 
-function toggleText() {
-  if (startStopBtn.classList.contains('active')){
-    startStopBtn.classList.remove('active')
-    startStopBtn.textContent = 'Pauza';
-  } else {
-    startStopBtn.classList.add('active')
-    startStopBtn.textContent = 'Start';
-  }
+const timer = () => {
+ if (active === false) {
+   active = true;
+   startStopBtn.textContent = 'Pauza';
+ } else {
+   active = false;
+   startStopBtn.textContent = 'Start';
+ }
+ //  startStopBtn.classList.toggle('active');
+ //  if (startStopBtn.classList.contains('active')){
+ //    startStopBtn.classList.remove('active');
+ //    startStopBtn.classList.
+ //    startStopBtn.textContent = 'Start';
+ //  } else {
+ //    startStopBtn.classList.add('active')
+ //    startStopBtn.textContent = 'Pauza';
+ //  }
 }
 
-startStopBtn.addEventListener('click', toggleText());
+startStopBtn.addEventListener('click', timer);
+//resetBtn.addEventListener('click', reset);
