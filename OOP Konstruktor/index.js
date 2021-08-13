c8c8093419b61e64aad53bd9471b54e0f5bff059
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-08-13 15:50:05 +0200

    Update index.js

--- OOP Konstruktor/index.js
+++ OOP Konstruktor/index.js
@@ -1,5 +1,19 @@
-function Car() {
-  console.log(this);
-}
+{
+  function Car(name) {
+    //console.log(this);
+    this.name = name;
+  }
 
-const maluch = new Car();
+  const maluch = new Car();
+  const polonez = new Car();
+
+  //1. nowy pusty obiekt
+  //2. wiązanie this z utworzonym obiektem
+  //3. powstaje właściwość __proto__
+  //4. Wywołanie funkcji i przypisanie do zmiennej
+
+  maluch.name = 'zabytek';
+
+  console.log(maluch);
+  console.log(polonez);
+}
