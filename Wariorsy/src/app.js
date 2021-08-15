cf306c496136196fc14ef3ec2e897ab7da8cbafa
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-08-15 14:37:27 +0200

    12

--- Wariorsy/src/app.js
+++ Wariorsy/src/app.js
@@ -1,35 +1,4 @@
-class Warrior {
-  #name;
-  #attack;
-  #hitPoints;
-
-  constructor(name, attack, hitPoints) {
-    this.#name = name;
-    this.#attack = attack;
-    this.#hitPoints = hitPoints;
-  }
-
-  getName() {
-    return this.#name;
-  }
-
-  getAttack() {
-    return this.#attack;
-  }
-
-  getHitPoints() {
-    return this.#hitPoints;
-  }
-
-  setHitPoints(hitPoints) {
-    this.#hitPoints = hitPoints;
-  }
-
-  levelUp() {
-    this.#hitPoints *= 1.1;
-    this.#attack *= 1.1;
-  }
-}
+const { Warrior } = require('./warrior')
 
 class Arena {
   #warrior1;
