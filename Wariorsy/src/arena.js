2d110310d688c4050655e7e08a201d66609fa487
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-08-15 14:57:46 +0200

    Update arena.js

--- Wariorsy/src/arena.js
+++ Wariorsy/src/arena.js
@@ -1,13 +1,23 @@
+import { Warrior } from './warrior';
+
 class Arena {
   #warrior1;
+
   #warrior2;
 
   constructor(warior1, warior2) {
+    if (!(warrior1 instanceof Warrior)) { throw new Error('warrior shuld be instance of Warrior class!'); }
+    if (!(warrior2 instanceof Warrior)) { throw new Error('warrior shuld be instance of Warrior class!'); }
     this.#warrior1 = warior1;
     this.#warrior2 = warior2;
+  }
+  fight() {
+    this.#warrior2.setHitPoints(
+      this.#warrior1.getAttack() - this.#warrior1.getAttack(),
+    );
   }
 }
 
 module.exports = {
   Arena,
-}
+};
