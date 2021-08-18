a232279726ebd6a546cabebd49652a67c701f8fd
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-08-18 16:04:37 +0200

    Update index.js

--- Etap2Dz3/index.js
+++ Etap2Dz3/index.js
@@ -1,10 +1,25 @@
 {
   //Callbacki w stylu Node (error, dane)
   const {readFile} = require('fs');
-  console.log('Hello World');
+  //console.log('Hello World');
 
   readFile('./index.js', 'utf8',(err, data) => {
-    console.log(data);
+    //console.log(data);
+  })
+
+  //console.log('Koniec programu.')
+}
+
+{
+  const {readFile} = require('fs');
+  console.log('Hello World');
+
+  readFile('./index.js', 'utf8',(error, data) => {
+    if (error === null) {
+      console.log(data);
+    } else {
+      console.log('Oh no!', error);
+    }
   })
 
   console.log('Koniec programu.')
