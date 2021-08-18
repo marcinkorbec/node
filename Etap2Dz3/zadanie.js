c3575fb0b80346e16c97b19926de77d944239190
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-08-18 17:13:55 +0200

    Update zadanie.js

---
+++ Etap2Dz3/zadanie.js
@@ -0,0 +1,6 @@
+// istnieje moduł dns, który służy m in do operacji związanych z serwerami nazw.
+// istnieje w nim funkcja dns.lookup(host), która jest asynchroniczna.
+// Spróbuj sprawdzić dzięki temu jaki jest adres IP hosta google,com.
+// -Wykonaj zadanie na callbackach.
+// -Następnie wykorzystaj util.promisify().
+// -Na końcu spróbuj wykorzystać specjalną wersje dostępną w dns.promises async/await.
