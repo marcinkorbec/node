c5dc6d340e6ca3e69d92f6134545f697e0985e7b
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-08-18 16:34:04 +0200

    Update index.js

--- Etap2Dz3/index.js
+++ Etap2Dz3/index.js
@@ -1,26 +1,26 @@
 {
   //Callbacki w stylu Node (error, dane)
-  const {readFile} = require('fs');
-  //console.log('Hello World');
-
-  readFile('./index.js', 'utf8',(err, data) => {
-    //console.log(data);
-  })
-
-  //console.log('Koniec programu.')
+  // const {readFile} = require('fs');
+  // //console.log('Hello World');
+  //
+  // readFile('./index.js', 'utf8',(err, data) => {
+  //   //console.log(data);
+  // })
+  //
+  // //console.log('Koniec programu.')
 }
 
 {
-  const {readFile} = require('fs');
-  //console.log('Hello World');
-
-  readFile('./indexwe.js', 'utf8',(error, data) => {
-    if (error === null) {
-      //console.log(data);
-    } else {
-      //console.log('Oh no!', error);
-    }
-  })
+  // const {readFile} = require('fs');
+  // //console.log('Hello World');
+  //
+  // readFile('./indexwe.js', 'utf8',(error, data) => {
+  //   if (error === null) {
+  //     //console.log(data);
+  //   } else {
+  //     //console.log('Oh no!', error);
+  //   }
+  // })
 
   //console.log('Koniec programu.')
 }
@@ -28,18 +28,54 @@
 //PRZEROBIENIE CALLBACKA W STYLU NODOWYM NA ZWYKŁEGO PROMISA
 
 {
-  const {readFile} = require('fs');
-  const {promisify} = require('util');
+  {
+    // const {readFile} = require('fs');
+    // const {promisify} = require('util');
+    // const readFilePromised = promisify(readFile);
+    // //console.log('Hello World');
+    //
+    // readFilePromised('./index.js', 'utf8')
+    //   .then(data => {
+    //     console.log(data);
+    //   })
+    //   .catch(error => {
+    //     console.log('Oh no', error);
+    //   });
+    //
+    // //console.log('Koniec programu.')
+  }
+  {
+    // const {readFile} = require('fs');
+    // const {promisify} = require('util');
+    // const readFilePromised = promisify(readFile);
+    //
+    // console.log('Hello World');
+    //
+    // (async () => {
+    //   const data = await readFilePromised('./index12.js', 'utf8',);
+    //   console.log(data);
+    // })();
+    //
+    // console.log('Koniec programu.')
+  }
+  {
+    const {readFile} = require('fs');
+    const {promisify} = require('util');
+    const readFilePromised = promisify(readFile);
 
-  //console.log('Hello World');
+    console.log('Hello World');
 
-  const readFilePromised = promisify(readfile);
-    .then(data => {
-      console.log(data);
-    })
-    .catch(error => {
-      console.log('Oh no', error);
-    });
+    (async () => {
+      try {
+        const data = await readFilePromised('./index12.js', 'utf8',);
+        console.log(data);
+      } catch (err) {
+        console.log('oh no kurwa', err);
+      }
+    })();
 
-  //console.log('Koniec programu.')
+    console.log('Koniec programu.')
+  }
+
+
 }
