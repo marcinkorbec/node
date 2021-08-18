040f9809643a6b32be74197a9fd0fdf37f82a907
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-08-18 17:05:38 +0200

    12

--- Etap2Dz3/index.js
+++ Etap2Dz3/index.js
@@ -59,15 +59,31 @@
     // console.log('Koniec programu.')
   }
   {
-    const {readFile} = require('fs');
-    const {promisify} = require('util');
-    const readFilePromised = promisify(readFile);
+    // const {readFile} = require('fs');
+    // const {promisify} = require('util');
+    //
+    // console.log('Hello World');
+    //
+    // (async () => {
+    //   try {
+    //     const data = await promisify(readFile)('./index.js', 'utf8',);
+    //     console.log(data);
+    //   } catch (err) {
+    //     console.log('oh no kurwa', err);
+    //   }
+    // })();
+    //
+    // console.log('Koniec programu.')
+  }
+  {
+    const {readFile} = require('fs').promises;
+
 
     console.log('Hello World');
 
     (async () => {
       try {
-        const data = await readFilePromised('./index12.js', 'utf8',);
+        const data = await readFile('./index.js', 'utf8',);
         console.log(data);
       } catch (err) {
         console.log('oh no kurwa', err);
@@ -76,6 +92,5 @@
 
     console.log('Koniec programu.')
   }
-
 
 }
