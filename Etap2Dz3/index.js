03aebdd4895171b4fe8ad2046e7990313c5aa5eb
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-08-18 16:14:40 +0200

    Update index.js

--- Etap2Dz3/index.js
+++ Etap2Dz3/index.js
@@ -29,15 +29,17 @@
 
 {
   const {readFile} = require('fs');
+  const {promisify} = require('util');
+
   //console.log('Hello World');
 
-  readFile('./indexwe.js', 'utf8',(error, data) => {
-    if (error === null) {
-      //console.log(data);
-    } else {
-      //console.log('Oh no!', error);
-    }
-  })
+  const readFilePromised = promisify(readfile);
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.log('Oh no', error);
+    });
 
   //console.log('Koniec programu.')
 }
