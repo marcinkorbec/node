f64985845e78cd1a2862b74fab782b33e4fa5646
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-08-20 14:59:03 +0200

    Update index.js

--- 20.08.2021/index.js
+++ 20.08.2021/index.js
@@ -69,14 +69,45 @@
 // readFileAndDirectories();
 
 //---------------------------------------------------------------------------------------------------
+//Tego nie stosować bo w dokumentacji jest depreciated i że zaraz będzie wycofane i dlatego nie działa
+// const {exists} = require('fs').promises;
+// const FILE_NAME =  './data/hello-world.txt';
+//
+// (async () => {
+//   const fileExist = await exists(FILE_NAME);
+//   if (!fileExist) {
+//     console.log('This is not available file.')
+//   }
+// })();
 
-const {exists} = require('fs').promises;
-const FILE_NAME =  './data/hello-world.txt';
+//-----------------------------------------------------------------------------------------------------
+// Tego nie zalecają robić bo między 90 a 94 linijką na komputerze mogło coś się zmienić
+// const {access readFile} = require('fs').promises;
+// const FILE_NAME =  './data/hello-world.txt';
+//
+// (async () => {
+//   try {
+//     await access(FILE_NAME, W_OK);
+//   } catch(err) {
+//     console.log('Wystąpił błąd, nie ma takiego pliku.')
+//   }
+//   readFile(FILE_NAME);
+// })();
+
+//-------------------------------------------------------------------------------------------------------
+
+const {readFile} = require('fs').promises;
+const FILE_NAME = './data/hello.txt';
 
 (async () => {
-  const fileExist = await exists(FILE_NAME);
-  if (!fileExist) {
-    console.log('This is not available file.')
+  try {
+    const result = await readFile(FILE_NAME, 'utf8');
+    console.log(result);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      console.log('Plik nie istnieje');
+    } else {
+      console.log('Nieznany błąd wyskoczył.',error);
+    }
   }
 })();
-
