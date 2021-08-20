47d58e82e79bbbed262021165142fb26775ca8b1
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-08-20 13:38:31 +0200

    add

--- 20.08.2021/index.js
+++ 20.08.2021/index.js
@@ -1,8 +1,40 @@
-const {readdir} = require('fs').promises;
+// const {readdir} = require('fs').promises;
+//
+// async function readFileAndDirectories() {
+//   const list = await readdir('.');
+//   console.log(list);
+// }
+//
+// readFileAndDirectories();
+
+//------------------------------------------------------------------------------------------------------------
+
+// const {readdir, readFile} = require('fs').promises;
+//
+// async function readFileAndDirectories() {
+//   const fileNames = await readdir('./data');
+//
+//   for(const fileName of fileNames) {
+//     console.log(fileName);
+//     const fileContent = await readFile(`./data/${fileName}`, 'utf8');
+//     console.log(fileContent);
+//   }
+// }
+//
+// readFileAndDirectories();
+
+//------------------------------------------------------------------------------------------------------------
+
+const {readdir, readFile, stat} = require('fs').promises;
 
 async function readFileAndDirectories() {
-  const list = await readdir('.');
-  console.log(list);
+  const fileNames = await readdir('./data');
+
+  for(const fileName of fileNames) {
+    console.log(fileName);
+    const fileStat = await stat(`./data/${fileName}`);
+    console.log(fileStat);
+  }
 }
 
 readFileAndDirectories();
