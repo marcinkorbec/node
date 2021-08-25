9f3e788a8571245762fe0ea249efb088ebb888e6
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-08-25 12:20:46 +0200

    Update index.js

---
+++ 25.08.2021/index.js
@@ -0,0 +1,11 @@
+// PACZKI EKOSYSTEMU NODE.JS
+
+//  Alternatywa dla NPM - Yarn
+
+//  Po co?
+//   - prędkość
+//   - jeszcze szybsze komendy.
+
+// W przeciwnieństwie do npm, Yarn nie jest automatycznie zainstalowany. Można go pobrać jako instalator,
+// ale od niedawna można go zainstaować npm-em :)
+// npm install --global yarn
