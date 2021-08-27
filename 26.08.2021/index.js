63b9a9319ae52f9ba283881b981e3b7423507ccd
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-08-27 15:06:01 +0200

    add

---
+++ 26.08.2021/index.js
@@ -0,0 +1,10 @@
+// npm i -D @types/node-fetch instalacja typów
+
+const fetch = require('node-fetch');
+
+// fetch('https://wp.pl')
+//   .then(r => r.text())
+//   .then(html => console.log(html));
+
+
+
