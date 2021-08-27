19eda42a656aa95db9de6338370dfd300bea70b1
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-08-27 14:57:10 +0200

    add
    add

--- 25.08.2021/index.js
+++ 25.08.2021/index.js
@@ -9,3 +9,22 @@
 // W przeciwnieństwie do npm, Yarn nie jest automatycznie zainstalowany. Można go pobrać jako instalator,
 // ale od niedawna można go zainstaować npm-em :)
 // npm install --global yarn
+
+//Komendy yarn
+//yarn add (...)
+//yarn add cośtam@verion
+//yarn remove (...)
+
+// Śłedzenie plików z chokidar
+// - można reagować na zmiany w kodzie nawet w zewnętrznych usługach.
+// - możesz reagować na pliki wysyłane przez klientów np. za pomocą ftp na lokalny serwer.
+// - Możesz stworzyć narzędzie logujące zmiany w systemie.
+
+//---------------------------------------------------------------------------------------------------------
+
+const {watch} = require ('chokidar');
+
+watch('.')
+  .on('all', (event, patch) => {
+    console.log(event, patch);
+  })
