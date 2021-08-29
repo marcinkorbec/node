b4b15a40846dc947705461c2b503278b29301c49
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-08-29 15:59:26 +0200

    add

--- 26.08.2021/index.js
+++ 26.08.2021/index.js
@@ -1,6 +1,8 @@
 // npm i -D @types/node-fetch instalacja typów
 
 const fetch = require('node-fetch');
+const { appendFile } = require('fs').promises;
+const { normalize, resolve } = require('path');
 
 // fetch('https://wp.pl')
 //   .then(r => r.text())
@@ -8,11 +10,18 @@ const fetch = require('node-fetch');
 
 const cityName = 'Rzeszów'; //process.argv[2];
 
+function safeJoin(base, target) {
+  const targetPatch = '.' + normalize('/' + target)
+  return resolve(base, targetPatch);
+}
+
+const getDataFileName = city => safeJoin('.', `${city}.txt`);
+
 // fetch('https://danepubliczne.imgw.pl/api/data/synop')
 //   .then(r => r.json())
 //   .then(data => console.log(data));
 
-const processWeatcherData = data => {
+const processWeatcherData = async data => {
   // const data.find(stationData => {
   //   if (stationData.stacja === cityName) {
   //     return true;
@@ -36,8 +45,13 @@ const processWeatcherData = data => {
   const weatherInfo = `W mieście ${cityName} jest: ${temperature}°C, wilgotność: ${humidity}, oraz ciśnienie: ${pressure} hPa.`
 
   console.log(weatherInfo);
+  const dateTimeString = new Date().toLocaleString();
+  await appendFile(getDataFileName(cityName), `${dateTimeString}\n${weatherInfo}\n`)
 }
 
 fetch('https://danepubliczne.imgw.pl/api/data/synop')
   .then(r => r.json())
   .then(processWeatcherData);
+  .catch(error => {
+  console.log('Error has occured :D', error);
+})
