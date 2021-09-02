a1d3bedca32e2a9e2e6e85bb9c55adc68ec4c820
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-09-02 18:08:55 +0200

    Update index.js

--- 24-08-2021/index.js
+++ 24-08-2021/index.js
@@ -1,1 +1,18 @@
-const
+const {readdir} = require('fs').promises;
+
+(async() => {
+
+  const oldFile = process.argv[2];
+  const newFile = process.argv[3];
+
+  try {
+    await rename(oldFile, newFile);
+  } catch (e) {
+    if (e.code === 'ENOENT') {
+      console.log(`${oldFile} does not exist.`);
+    } else {
+      
+    }
+  }
+
+})();
