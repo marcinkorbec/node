5b33a9ff04c2494d2345e51e28ebfc7dceb002f2
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-09-05 13:43:03 +0200

    Update index.js

--- 25.08.2021/index.js
+++ 25.08.2021/index.js
@@ -31,7 +31,7 @@ function safeJoin(base, target){
   return resolve (base, targetPatch);
 }
 
-// jeja raz dwa trzy cztery 
+// jeja raz dwa trzy czter
 const userPath = safeJoin(__dirname,  process.argv[2] ? process.argv[2] : '.');
 
 console.log('Starting watch: ', resolve(userPath));
