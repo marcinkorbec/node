431f3d542bfd175f6df9ee95f71c628e208a9648
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-09-06 15:37:29 +0200

    Update index.js

--- 1.09.2021/index.js
+++ 1.09.2021/index.js
@@ -1,21 +1,34 @@
-const { promisify } = require('util');
-const scrypt = promisify(require('crypto').randomBytes);
-const { createCipheriv } = require('crypto');
+{
+  const {promisify} = require('util');
+  const scrypt = promisify(require('crypto').randomBytes);
+  const {createCipheriv} = require('crypto');
 
-const algorithm = 'aes-192-cbc'; //AES 192
-const password = 'Password used to generate key';
+  const algorithm = 'aes-192-cbc'; //AES 192
+  const password = 'Password used to generate key';
 
-(async () => {
-  //First, we'll generate the key.The key lenghth is dependent on the algorithm.
-  //In this case for aes192, it is 24bytes (192 bits).
-  const key = await scrypt(password, 'salt', 24);
-  const iv = await randomBytes(16);
+  (async () => {
+    //First, we'll generate the key.The key lenghth is dependent on the algorithm.
+    //In this case for aes192, it is 24bytes (192 bits).
+    const key = await scrypt(password, 'salt', 24);
+    const iv = await randomBytes(16);
 
-  const cipher = createCipheriv(algorithm, key, iv);
-  let encrypted = cipher.update('some clear text data', 'utf8', 'hex');
-  encrypted += cipher.final('hex');
-  console.log({
-    encrypted,
-    iv: iv.toString('hex'),
-  });
-})();
+    const cipher = createCipheriv(algorithm, key, iv);
+    let encrypted = cipher.update('some clear text data', 'utf8', 'hex');
+    encrypted += cipher.final('hex');
+    console.log({
+      encrypted,
+      iv: iv.toString('hex'),
+    });
+  })();
+}
+
+{
+  const { promisify } = require('util');
+  const scrypt = promisify(require('crypto').scrypt);
+  const { createDecipheriv } = require('crypto');
+
+  const algorithm = 'aes-192-cbc';
+  const password = 'Password used to generate key';
+  const encrypted = 'eed497f53ea697fa05584c34a54ae04c60d3169d47fabedc04c5fe797550aeed';
+  const ivHex = '976c423614aea8cfad695a735b321255';
+}
