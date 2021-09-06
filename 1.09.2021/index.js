27c1e5f2599e879dd628223677859891e28de50b
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-09-06 15:43:53 +0200

    Update index.js

--- 1.09.2021/index.js
+++ 1.09.2021/index.js
@@ -31,4 +31,16 @@
   const password = 'Password used to generate key';
   const encrypted = 'eed497f53ea697fa05584c34a54ae04c60d3169d47fabedc04c5fe797550aeed';
   const ivHex = '976c423614aea8cfad695a735b321255';
+
+  (async () => {
+    //First, we'll generate the key. The key length is dependent on the algorithm.
+    //In this case for aes192, it is 24 bytes (192 bits).
+    const key = await scrypt(password, 'salt', 24);
+    const iv = Buffer.from(ivHex, 'hex'); //Initialization vector.
+
+    const decipher = createDecipheriv(algorithm, key, iv);
+    let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    console.log(decrypted);
+  })();
 }
