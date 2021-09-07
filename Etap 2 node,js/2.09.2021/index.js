76ef91e771273f1faf4647fb24fad80946627318
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-09-07 11:01:21 +0200

    Update index.js

--- Etap 2 node,js/2.09.2021/index.js
+++ Etap 2 node,js/2.09.2021/index.js
@@ -1,6 +1,8 @@
 const {createHmac} = require('crypto');
+
 const salt = '%^&*hgvcbsekuhfed7qe386e8';
-const hash = crypto.createHmac('sha512', salt)
+
+const hash = createHmac('sha512', salt)
   .update('Tekst do shashowania')
   .digest('hex');
 
