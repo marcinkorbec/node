b3fea5a12c06e841d7a8f13adccb1d751bfc6806
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-09-07 11:14:59 +0200

    Update index.js

--- Etap 2 node,js/2.09.2021/index.js
+++ Etap 2 node,js/2.09.2021/index.js
@@ -7,3 +7,12 @@
 //   .digest('hex');
 //
 // console.log(hash);
+
+const { pbkdf2 } = require('crypto');
+const originalText = 'Tekst do zhashowania';
+const SALT = '847ruegf76fog8ngfcofoefgi7wggf wgfo'
+
+pbkdf2(originalText, SALT, 100000, 64, 'sha512', (err,derivedKey)=> {
+  if (err) throw err;
+  console.log(derivedKey.toString('hex'));
+});
