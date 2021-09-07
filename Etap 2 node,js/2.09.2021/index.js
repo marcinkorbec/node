bbd8716c685c15fb0902036fa4247457b10a164f
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-09-07 11:07:00 +0200

    Update index.js

--- Etap 2 node,js/2.09.2021/index.js
+++ Etap 2 node,js/2.09.2021/index.js
@@ -1,9 +1,9 @@
-const {createHmac} = require('crypto');
-
-const salt = '%^&*hgvcbsekuhfed7qe386e8';
-
-const hash = createHmac('sha512', salt)
-  .update('Hello World from MEGAKURS!')
-  .digest('hex');
-
-console.log(hash);
+// const {createHmac} = require('crypto');
+//
+// const salt = '%^&*hgvcbsekuhfed7qe386e8';
+//
+// const hash = createHmac('sha512', salt)
+//   .update('Hello World from MEGAKURS!')
+//   .digest('hex');
+//
+// console.log(hash);
