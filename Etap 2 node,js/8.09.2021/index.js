6bfb2591902549a4eefc091d35009567df09c877
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-09-09 00:04:13 +0200

    add

---
+++ Etap 2 node,js/8.09.2021/index.js
@@ -0,0 +1,9 @@
+// const {readFile} = require('fs').promises;
+//
+// (async () => {
+//   console.log(await readFile('./index.js'))
+// })();
+
+const buff = Buffer.alloc(20);
+
+console.log(buff);
