226e38c42641d66ed97772378e056e15b8e80030
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-09-08 16:24:10 +0200

    add

--- Etap 2 node,js/4.09.2021/projekt_decrypt.js
+++ Etap 2 node,js/4.09.2021/projekt_decrypt.js
@@ -3,5 +3,8 @@ const {decryptText} = require('cipher.js');
 const [,,fileName, pwd] = process.argv;
 
 (async () => {
-  const json = await readFile
+  const json = await readFile(fileName, 'utf8')
+  const encrypted = JSON.parse(json);
+  console.log(encrypted);
+  const decrypted = await decryptText(encrypted.encrypted, pwd, )
 })();
