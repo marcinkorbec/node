30e4643b25744ec2b229dfc5a6fd04c88eb091a8
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-09-09 12:37:08 +0200

    Update index.js

--- Etap 2 node,js/8.09.2021/index.js
+++ Etap 2 node,js/8.09.2021/index.js
@@ -13,4 +13,7 @@ const write = buff.write('Zażółć gęślą jaźń!', 'utf8')
 // console.log(buff.toString('hex'));
 // console.log(write);
 
-Buffer.byteLength('Zażółć gęślą jaźń!')
+console.log(Buffer.byteLength('Zażółć gęślą jaźń!'));
+console.log(Buffer.byteLength('a'));
+console.log(Buffer.byteLength('ą'));
+console.log(Buffer.byteLength('ć'));
