0b78bb3a372e9d5855af6bea41e1feefb3bd090a
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-09-10 00:14:55 +0200

    add

--- Etap 2 node,js/9.09.2021/index.js
+++ Etap 2 node,js/9.09.2021/index.js
@@ -1,3 +1,32 @@
-const {createReadSteram} = require('fs');
+// const {createReadStream, createWriteStream} = require('fs');
+// const {pipeline} = require('stream').promises;
+//
+//
+// (async () => {
+//   await pipeline (
+//     createReadStream('picture.png'),
+//     createWriteStream('picture2.png'),
+//   );
+//
+//   console.log('Done!');
+// })();
 
-const createReadSteram()
+// const {createReadStream, createWriteStream} = require('fs');
+//
+// const r = createReadStream('picture.png');
+// const w = createWriteStream('picture2.png');
+//
+// r.pipe(w);
+// r.on('end', ()=> console.log('Ready'));
+
+const {createReadStream, createWriteStream} = require('fs');
+
+const r = createReadStream('picture.png');
+const w = createWriteStream('picture2.png');
+
+r.on('data', data=> w.write(data));
+r.on('end', ()=> {
+  w.close();
+  console.log('Ready!')
+});
+
