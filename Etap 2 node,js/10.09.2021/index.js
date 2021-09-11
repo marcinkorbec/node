ebdf58204da7de3fe6bc5006f641669ecb5b275c
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-09-11 21:57:48 +0200

    add

--- Etap 2 node,js/10.09.2021/index.js
+++ Etap 2 node,js/10.09.2021/index.js
@@ -1,13 +1,25 @@
+// const {createReadStream, createWriteStream} = require('fs');
+// const {pipeline} = require('stream'). promises;
+// const {createGzip} = require('zlib');
+//
+// (async () => {
+//   await pipeline(
+//     createReadStream('picture.png'),
+//     createGzip(),
+//     createWriteStream('picture2.png'),
+//   );
+//   console.log('Done!');
+// })();
+
 const {createReadStream, createWriteStream} = require('fs');
 const {pipeline} = require('stream'). promises;
 const {createGzip} = require('zlib');
 
 (async () => {
   await pipeline(
-    createReadStream('picture.png'),
+    createReadStream('picture2.png'),
     createGzip(),
-    createWriteStream('picture2.png'),
+    createWriteStream('picture3.png'),
   );
   console.log('Done!');
 })();
-
