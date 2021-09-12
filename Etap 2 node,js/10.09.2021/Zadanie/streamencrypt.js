61fdbae838556a2ad5e8e02aecb389579748db46
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-09-12 12:18:27 +0200

    Update streamencrypt.js

--- Etap 2 node,js/10.09.2021/Zadanie/streamencrypt.js
+++ Etap 2 node,js/10.09.2021/Zadanie/streamencrypt.js
@@ -1,12 +1,21 @@
+const { promisify } = require('util');
+const scrypt = promisify(require('crypto').scrypt);
 const {createReadStream, createWriteStream} = require('fs');
 const {pipeline} = require('stream').promises;
+const {ENCRYPTED_SALT} = require('./constants');
+const {createCipher} = require('crypto'); //A w drugim createDecipher
+
 
 
 (async () => {
   const [,, inputFile, outputFile, pwd] = process.argv;
+  const key = await scrypt(pwd, ENCRYPTED_SALT, 24);
+  const algorithm = 'aes-192-cbc';
+
   await pipeline (
     createReadStream(inputFile),
-    //szyfrowanie
+    let encrypted = cipher.update(text, 'utf8', 'hex');
+    const cipher = createCipher(algorithm, key);
     createWriteStream(outputFile),
   );
 
