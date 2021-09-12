9d5364c7159e941dae415b97ff2c0d4cfecc39d1
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-09-12 11:52:07 +0200

    Update streamencrypt.js

--- Etap 2 node,js/10.09.2021/Zadanie/streamencrypt.js
+++ Etap 2 node,js/10.09.2021/Zadanie/streamencrypt.js
@@ -1,4 +1,5 @@
 const {createReadStream, createWriteStream} = require('fs');
+const {pipeline} = require('stream').promises;
 
 const openFileStream = createReadStream('package.json');
 const writeFileStream = createWriteStream('encrypted.json);
