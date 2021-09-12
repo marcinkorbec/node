f1eb5358e4df8a3965002e5fdf465c3f12b2208e
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-09-12 13:35:40 +0200

    add

---
+++ Etap 2 node,js/10.09.2021/Zadanie/streamdecrypt.js
@@ -0,0 +1,21 @@
+const {pipeline} = require('stream').promises;
+const { promisify } = require('util');
+const scrypt = promisify(require('crypto').scrypt);
+const {createReadStream, createWriteStream} = require('fs');
+const {createDecipher} = require('crypto'); //A w drugim createDecipher
+const {ENCRYPTED_SALT} = require('./constants');
+
+
+(async () => {
+  const [,, inputFile, outputFile, pwd] = process.argv;
+  const key = await scrypt(pwd, ENCRYPTED_SALT, 24);
+  const algorithm = 'aes-192-cbc';
+
+  await pipeline (
+    createReadStream(inputFile),
+    createDecipher(algorithm, key),
+    createWriteStream(outputFile),
+  );
+
+  console.log('Done!');
+})();
