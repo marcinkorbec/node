bb49d885d3233e5402a7296cd42a074cc7a12f34
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-09-14 14:18:40 +0200

    Update tick-tock.js

--- Etap 2 node,js/13.09.2021/tick-tock.js
+++ Etap 2 node,js/13.09.2021/tick-tock.js
@@ -1,4 +1,23 @@
+// const {EventEmitter} = require('events');
+//
+// function tickTock() {
+//   const ee = new EventEmitter();
+//   setInterval(() => {
+//     ee.emit('secondElapsed', 'test');
+//   }, 1000);
+//   setInterval(() => {
+//     ee.emit('fiveSecondElapsed', 'test');
+//   }, 5000);
+//   return ee;
+// };
+//
+// module.exports = {
+//   tickTock,
+// };
+
 const {EventEmitter} = require('events');
+
+class TickTock extends EventEmitter
 
 function tickTock() {
   const ee = new EventEmitter();
