3d61710478f64ca3b5dcca00602672834da4bd2c
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-09-14 13:38:04 +0200

    Update index.js

--- Etap 2 node,js/13.09.2021/index.js
+++ Etap 2 node,js/13.09.2021/index.js
@@ -1,9 +1,9 @@
 const {tickTock} = require('./tick-tock')
 const events = tickTock();
 events.on('secondElapsed', data => {
-  console.log('mineła sekunda!', data)
+  console.log('1 sec!', data)
 });
 
-events.on('secondElapsed', data => {
-  console.log('Tick!', data)
+events.on('fiveSecondElapsed', data => {
+  console.log('5 sec!', data)
 });
