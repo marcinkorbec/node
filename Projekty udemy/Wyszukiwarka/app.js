2d95ad84d2f933f6e0d52f042ad5cce2933c044d
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-09-15 22:01:25 +0200

    Update app.js

--- Projekty udemy/Wyszukiwarka/app.js
+++ Projekty udemy/Wyszukiwarka/app.js
@@ -2,10 +2,12 @@ const search = document.querySelector('.search');
 const li = document.querySelectorAll('li');
 
 const searchEngine = (e) => {
-  const text = e.target.value;
-  
+  const text = e.target.value.toLowerCase();
+
   li.forEach(el => {
-    if (el.textContent !== -1) {
+    const task = el.textContent;
+
+    if (task.toLowerCase().indexOf(text) !== -1) {
       el.style.display = 'block';
     } else {
       el.style.display = 'none';
