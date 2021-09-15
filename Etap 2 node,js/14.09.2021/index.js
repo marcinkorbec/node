408503d0fee5c74f4b7903b5b6abc49c7053a678
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-09-15 13:48:53 +0200

    Update index.js

--- Etap 2 node,js/14.09.2021/index.js
+++ Etap 2 node,js/14.09.2021/index.js
@@ -13,3 +13,24 @@
 // });
 //
 // server.listen(3000, 'localhost');
+
+//----------------------------------------------------------------------------------------------------------------------
+
+const {createServer} = require('http');
+
+const server = createServer();
+server.on('request', (req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+    });
+    res.end('<h1>Hello from Megakurs</h1>');
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+    });
+    res.end('<h1>Not Found</h1>');
+  }
+});
+
+server.listen(3000, 'localhost');
