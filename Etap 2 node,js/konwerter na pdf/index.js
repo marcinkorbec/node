ff9b652f90bd29f3361fae029f7764865d24b9c5
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-09-16 21:58:17 +0200

    add

--- Etap 2 node,js/konwerter na pdf/index.js
+++ Etap 2 node,js/konwerter na pdf/index.js
@@ -2,8 +2,11 @@ const fs = require("fs");
 const imgToPDF = require("image-to-pdf");
 
 fs.readdir("./zdjecia", ((err, data) => {
-  const directories = data.map((entry) => `./zdjecia/${entry}`);
+  const filesNumbers = data.map((e) => e.split("_")[1].split(".")[0]);
+  const sortedFilesNames = filesNumbers.sort((a, b) => a - b);
+  console.log(sortedFilesNames);
+  const directories = sortedFilesNames.map((entry) => `./zdjecia/Screenshot_${entry}.png`);
   console.log(directories);
-  imgToPDF(directories, "A4")
+  imgToPDF(directories, "HD")
     .pipe(fs.createWriteStream("./data-out/output.pdf"));
 }));
