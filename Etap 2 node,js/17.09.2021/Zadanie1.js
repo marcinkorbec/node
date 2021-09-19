477416c43185497830967e17627526dcb6b8e30a
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-09-19 16:28:00 +0200

    Update Zadanie1.js

--- Etap 2 node,js/17.09.2021/Zadanie1.js
+++ Etap 2 node,js/17.09.2021/Zadanie1.js
@@ -6,7 +6,7 @@ http.createServer( (req, res) => {
   counter = counter+= 1;
   console.log(counter);
   res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(`<h1>Witaj na stronie stworzonej w Node.js. Ilosc Twoich odwiedzin to ${counter} razy</h1>`)
+  res.end(`<meta charset="UTF-8"><h1>Witaj na stronie stworzonej w Node.js. Ilość Twoich odwiedzin to ${counter} razy</h1>`)
 
 
 }).listen(3000, '127.0.0.1');
