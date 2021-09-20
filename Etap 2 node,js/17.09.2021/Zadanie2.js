445f20b21f2644df69357c2da7d2c6a709cd549c
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-09-20 12:27:21 +0200

    add

--- Etap 2 node,js/17.09.2021/Zadanie2.js
+++ Etap 2 node,js/17.09.2021/Zadanie2.js
@@ -1,18 +1,16 @@
 const {createServer} = require('http');
-const {calc} = require('/calc')
+const {calc} = require('./calc');
 const server = createServer();
 
 server.on('request', (req, res) => {
   const [, operation, a, b] = req.url.split('/');
 
-  const result = calc(operation, a, b);
+  const result = calc(operation, Number(a), Number(b));
 
-  }
-  if (req.url === '/') {
-    res.writeHead(200, {
+  res.writeHead(200, {
       'conetent-type': 'text/plain',
     });
-  }
+  res.end( result + '');
 });
 
 server.listen(3000, '127.0.0.1');
--- Etap 2 node,js/17.09.2021/calc.js
+++ Etap 2 node,js/17.09.2021/calc.js
@@ -1,5 +1,24 @@
-function calc() {
+function calc(operation, a, b) {
+  // if (operation === 'multiply') {
+  //   return a * b;
+  // } else if (operation === 'add') {
+  //   return a + b;
+  // } else if (operation === 'substract') {
+  //   return a - b;
+  // } else if (operation === 'divide') {
+  //   return a / b;
+  // }
 
+  switch (operation) {
+    case 'add':
+      return a + b;
+    case 'multiply':
+      return a * b;
+    case 'substract':
+      return a - b;
+    case 'divide':
+      return a / b;
+  }
 }
 
 module.exports = {
