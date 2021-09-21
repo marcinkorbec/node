a0551d825c9763273e9a24df58c306c7d7a2eac9
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-09-21 17:53:53 +0200

    Update index.js

--- battery level/index.js
+++ battery level/index.js
@@ -1,7 +1,10 @@
 const batteryLevel = require('battery-level');
 
-batteryLevel().then(level => {
-  const batLevel = (level*100)
-  console.log(batLevel.toString() + '%');
+batteryLevel()
+  .then(level => {
+    const batLevel = (level*100);
+    console.log(batLevel.toString() + '%');
   //=> 0.55
-});
+})
+
+setInterval(batteryLevel, 1000);
