a4de7261fbe6532fba9cb2018befb7dcf11e4da7
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-10-15 20:25:57 +0200

    Update index.js

--- Etap 3 Express.js/14.10.2021/index.js
+++ Etap 3 Express.js/14.10.2021/index.js
@@ -1,2 +1,10 @@
 const express = require('express');
-const rateLimit = 
+const nameRouter = require('./routes/name');
+
+const app = express;
+
+app.use(express.json());
+app.use('/name', nameRouter);
+
+app.listen(3000, 'localhost');
+
