b666c53255862aa0bbcd82a726c30f1c85499bb2
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-10-15 21:06:28 +0200

    Update vote.js

--- Etap 3 Express.js/14.10.2021/routes/vote.js
+++ Etap 3 Express.js/14.10.2021/routes/vote.js
@@ -1,10 +1,24 @@
+const express = require('express');
 const voteRouter = express.Router();
+
+let votesOnYes = 0;
+let votesOnNo = 0;
 
 voteRouter
   .get('/yes', (req, res) => {
-
+    votesOnYes++;
+    res.send('Dziękujemy za głos!');
   })
 
   .get('/no', (req, res) => {
-
+    votesOnNo++;
+    res.send('Dziękujemy za głos!')
   })
+
+  .get('/check', (req, res) => {
+    res.send(`<p>Głosy na tak: ${votesOnYes}, głosy na nie: ${votesOnNo}.</p>`)
+  })
+
+module.exports = {
+  voteRouter,
+}
