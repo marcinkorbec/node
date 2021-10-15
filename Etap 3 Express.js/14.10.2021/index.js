f63c54b91b9bf5a054a4dca1216b294073474c90
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-10-15 20:51:09 +0200

    add

--- Etap 3 Express.js/14.10.2021/index.js
+++ Etap 3 Express.js/14.10.2021/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-//const nameRouter = require('./routes/name');
-const app = express;
+const nameRouter = require('./routes/name');
+const app = express();
 
 app.use(express.json());
 app.use(express.static('public'));
