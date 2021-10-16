2ff0c7dc1806e60691417bac28e4e55dbe443903
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-10-16 13:36:35 +0200

    add

--- Etap 3 Express.js/14.10.2021/routes/vote.js
+++ Etap 3 Express.js/14.10.2021/routes/vote.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const voteRouter = express.Router();
+const {IpRestrict} = require('../util/restrict');
 
 const votes = {
   tak: 0,
   nie: 0,
 }
-console.log(votes);
+
+const ipRestrict = new IpRestrict();
+
 voteRouter
   .get('/check', async (req, res) => {
     const info = Object.entries(votes).map(ar => `Głos na ${ar[0]}: ${ar[1]}`).join('<br>');
@@ -13,6 +16,9 @@ voteRouter
   })
 
   .get('/:voteName', async (req, res) => {
+    if (!ipRestrict.check(req.ip)) {
+      res.status(403).send('Głos oddano już wcześniej!')
+    }
     const {voteName} = req.params;
     if (typeof votes[voteName] === 'undefined') {
       votes[voteName] = 0;
