92ec39f7148a3eebdde5fee6f892829b78b0a5f9
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-10-16 14:08:56 +0200

    add

--- Etap 3 Express.js/15.10.2021/public/js/front.js
+++ Etap 3 Express.js/15.10.2021/public/js/front.js
@@ -1,2 +1,23 @@
 const numberAInput = document.querySelector('#num1');
 const numberBInput = document.querySelector('#num2');
+const form = document.querySelector('form');
+
+form.addEventListener('submit', async event => {
+  event.preventDefault();
+
+  const numberA = Number(numberAInput.value);
+  const numberB = Number(numberBInput.value);
+
+  const res = fetch('/calc/check', {
+    method: 'POST',
+    body: JSON.stringify({
+      numberA,
+      numberB,
+    }),
+    headers: {
+      'Content-type': 'application/json',
+    }
+  });
+  const data = await res.json();
+  console.log(data);
+});
