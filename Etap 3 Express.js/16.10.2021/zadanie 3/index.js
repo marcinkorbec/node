9b7136281f8b00dd06a9ff38d1889bd05fadfdec
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-10-16 23:38:36 +0200

    add

--- Etap 3 Express.js/16.10.2021/zadanie 3/index.js
+++ Etap 3 Express.js/16.10.2021/zadanie 3/index.js
@@ -7,10 +7,11 @@ const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 app.use(cookieParser());
-app.use('/cookie', cookieRouter);
+
 app.use(express.urlencoded({
   extended: true,
 }));
+app.use('/cookie', cookieRouter);
 
 app.listen(3000, 'localhost');
 
