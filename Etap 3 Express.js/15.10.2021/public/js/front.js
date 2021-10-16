22982e6623bb580cbc74860ab5dddfe17e08ce42
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-10-16 18:17:16 +0200

    add

--- Etap 3 Express.js/15.10.2021/public/js/front.js
+++ Etap 3 Express.js/15.10.2021/public/js/front.js
@@ -1,5 +1,6 @@
 const numberAInput = document.querySelector('#num1');
 const numberBInput = document.querySelector('#num2');
+const result = document.querySelector('#result');
 const form = document.querySelector('form');
 
 form.addEventListener('submit', async event => {
@@ -7,6 +8,9 @@ form.addEventListener('submit', async event => {
 
   const numberA = Number(numberAInput.value);
   const numberB = Number(numberBInput.value);
+
+  result.textContent = 'Loading...';
+  result.style.border = 'none';
 
   const res = await fetch('/calc/check', {
     method: 'POST',
@@ -19,6 +23,14 @@ form.addEventListener('submit', async event => {
     }
   });
   const data = await res.json();
-  console.log(data);
+
+
+  if (data.divider) {
+    result.innerText = 'Numer B jest dzielnikiem numeru A.';
+    result.style.border = '3px solid lime';
+  } else {
+    result.innerText = 'Numer B nie jest dzielnikiem numeru A.';
+    result.style.border = '3px solid red';
+  };
 
 });
