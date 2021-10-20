00f9b2c44d6b3888bb04ca449d51e787b4eae882
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-10-20 17:59:12 +0200

    add

--- Etap 3 Express.js/Konfigurator ciastek/index.js
+++ Etap 3 Express.js/Konfigurator ciastek/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const hbs = require('express-handlebars');
+const cookieParser = require('cookie-parser');
 const app - express();
 
+app.use(cookieParser());
 app.engine('.hbs', hbs({extname:'.hbs'}));
 app.set('view engine', '.hbs');
 
