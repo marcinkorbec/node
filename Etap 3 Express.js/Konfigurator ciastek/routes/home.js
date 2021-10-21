612933c903789f24b756f126b28d402b6f66a83b
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-10-21 16:43:44 +0200

    add

--- Etap 3 Express.js/Konfigurator ciastek/routes/home.js
+++ Etap 3 Express.js/Konfigurator ciastek/routes/home.js
@@ -4,7 +4,12 @@ const homeRouter = express.Router();
 homeRouter
 
   .get('/', (req, res) => {
-    res.render('home/index.hbs');
+    res.render('home/index.hbs', {
+      cookie: {
+        base: 'light',
+        addons: ['coconut'],
+      },
+    });
   });
 
 module.exports = {
