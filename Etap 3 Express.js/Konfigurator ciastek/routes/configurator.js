9998019384d9d5e6ab6c55c84f8c7e103c07545f
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-10-25 12:32:19 +0200

    add

--- Etap 3 Express.js/Konfigurator ciastek/routes/configurator.js
+++ Etap 3 Express.js/Konfigurator ciastek/routes/configurator.js
@@ -4,7 +4,6 @@ const configuratorRouter = express.Router();
 configuratorRouter
     .get('/select-base/:baseName', (req, res) => {
         const {baseName} = req.params;
-
         res
             .cookie('cookieBase', baseName)
             .render('configurator/base-selected.hbs', {
