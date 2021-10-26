43d61f94030ded3395112f7b48bf8db4070b918a
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-10-26 15:06:05 +0200

    Update order.js

--- Etap 3 Express.js/Konfigurator ciastek/routes/order.js
+++ Etap 3 Express.js/Konfigurator ciastek/routes/order.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const { getAddonsFromReq } = require("../utils/get-addons-from-req");
+const { handlebarsHelpers } = require("../utils/handlebars-helpers");
+const { COOKIE_BASES, COOKIE_ADDONS } = require("../data/cookie-data");
 const orderRouter = express.Router();
 
 orderRouter
+    .get('/order/summary', (req, res) => {
+        const {cookieBase, cookieAddons} = req.cookies;
+        const addons = getAddonsFromReq(req);
+        const sum = ( cookieBase ? handlebarsHelpers.findPrice(Object.entries(COOKIE_BASES), cookieBase) : 0)
+            + addons.reduce((prev, curr) => {
+                return prev + handlebarsHelpers.findPrice(Object.entries(COOKIE_ADDONS), curr)
+            }, 0);
+        res.render('order/summary', {
+            cookie: {
+                base: cookieBase,
+                addons: addons,
+            },
+            bases: Object.entries(COOKIE_BASES), // Tutaj zmieniamy to w tablice dwuwymiarowa zeby nie uzywac kluczy lecz samego indeksu
+            addons: Object.entries(COOKIE_ADDONS),
+            sum,
+        });
+    })
 
 module.exports = {
-  orderRouter,
+    orderRouter,
 }
