eb479522610f7621aa3abe092cacb6295af4cbe7
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-11-02 19:29:24 +0100

    Update configurator.js

--- Etap 3 Express.js/konfigurator ciastek wersja obiektowa/routes/configurator.js
+++ Etap 3 Express.js/konfigurator ciastek wersja obiektowa/routes/configurator.js
@@ -5,17 +5,19 @@ class ConfiguratorRouter {
     constructor(cmapp) {
         this.cmapp = cmapp;
         this.router = express.Router();
-        this.selectBase();
-        this.selectAddon();
-        this.deleteAddon();
+        this.setUpRoutes();
     }
 
-    selectBase = () => {
-    this.router.get('/select-base/:baseName', (req, res) => {
-        console.log(this)
+    setUpRoutes() {
+        this.router.get('/select-base/:baseName', this.selectBase);
+        this.router.get('/add-addon/:addonName', this.addAddon);
+        this.router.get('/delete-addon/:addonName', this.deleteAddon);
+    }
+    selectBase = (req, res) => {
+
         const {baseName} = req.params;
 
-            if(!COOKIE_BASES[baseName]) {
+            if(!this.cmapp.data.COOKIE_BASES[baseName]) {
                 return this.cmapp.renderError(res, 'Wybrałeś bazę, która nie istnieje!')
             } else {
                 res
@@ -24,45 +26,47 @@ class ConfiguratorRouter {
                         baseName,
                     })
             }
-        })
     }
 
-    selectAddon = () => {
-    this.router.get('/select-addon/:addonName', (req, res) => {
-            const {addonName} = req.params;
-            const addons = getAddonsFromReq(req);
+    selectAddon = (req, res) => {
 
-            if(!COOKIE_ADDONS[addonName]) { //sprawdzanie czy taki dodatek już istnieje
-                return this.cmapp.renderError(res, `Składnik ${addonName} został wybrany`);
-            } else if (addons.includes(addonName)) { //sprawdzanie czy dodatek został już dodany
-                return this.cmapp.renderError(res, `Składnik ${addonName} został już wybrany`);
-            } else { addons.push(addonName)
-                res
-                    .cookie('cookieAddons', JSON.stringify(addons))
-                    .render('configurator/add-ons.hbs', {
-                        addonName,
-                    })
-            }
-        })
-    }
+        const {addonName} = req.params;
 
-    deleteAddon = () => {
-    this.router.get('/delete-addon/:addonName', (req, res) => {
-            const {addonName} = req.params;
-            const oldAddons = getAddonsFromReq(req);
 
-            if (!oldAddons.includes(addonName)) {
+        if(!this.cmapp.data.COOKIE_ADDONS[addonName]) { //sprawdzanie czy taki dodatek już istnieje
+            return this.cmapp.renderError(res, `Składnik ${addonName} został wybrany`);
+        }
+
+        const addons = this.cmapp.getAddonsFromReq(req);
+
+        if (addons.includes(addonName)) { //sprawdzanie czy dodatek został już dodany
+            return this.cmapp.renderError(res, `Składnik ${addonName} został już wybrany`);
+        }
+        addons.push(addonName)
+            res
+                .cookie('cookieAddons', JSON.stringify(addons))
+                .render('configurator/add-ons.hbs', {
+                    addonName,
+                });
+
+    };
+
+    deleteAddon = (req, res) => {
+        const {addonName} = req.params;
+        const oldAddons = this.cmapp.getAddonsFromReq(req);
+
+
+        if (!oldAddons.includes(addonName)) {
                 return this.cmapp.renderError(res, 'Próbujesz usunąć dodatek, który nie był dodany!')
-            } else {
-                const addons = getAddonsFromReq(req).filter(addon => addon !== addonName);
-                res
-                    .cookie('cookieAddons', JSON.stringify(addons))
-                    .render('configurator/deleted.hbs', {
-                        addonName,
-                    })
-            }
-        });
-    }
+        }
+
+        const addons = oldAddons.filter(addon => addon !== addonName);
+        res
+            .cookie('cookieAddons', JSON.stringify(addons))
+            .render('configurator/deleted.hbs', {
+                addonName,
+            })
+        }
 }
 
 
