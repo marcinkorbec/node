de563dede1c7e2511ded0bae3315ae4f1ce5ce37
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-11-05 10:29:46 +0100

    add

---
+++ Etap 3 Express.js/Express zadanie podsumowujące/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+const {urlencoded} = require("express");
+const app = express();
+
+app.use(express.urlencoded({
+	extended: true
+}));
+app.use(express.static('public'));
+app.use(express.json());
+app.engine('.hbs', hbs({
+	extname: '.hbs',
+	helpers: 'handlebarsHelpers'
+}));
+app.set('view engine', '.hbs');
+
+app.listen(3000, '0.0.0.0', () => {
+	console.log('Program działa na porcie http://localhost:3000');
+});
