08a2d2b384c82a4f5bbde62f24fd54a3fb2c9d55
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-11-05 20:25:56 +0100

    add

--- Etap 3 Express.js/Express_zadanie_podsumowujace/routers/client.js
+++ Etap 3 Express.js/Express_zadanie_podsumowujace/routers/client.js
@@ -3,8 +3,24 @@ const clientRouter = express.Router();
 
 clientRouter
 	.get('/', (req, res) => {
-		res.render('layouts/test.hbs');
-	});
+		res.send('Pobierz Wszystkie');
+	})
+
+	.get('/:id', (req, res) => {
+		res.send('Pobierz pojedynczego!')
+	})
+
+	.put('/:id', (req, res) => {
+		res.send('Zmodyfikuj pojedynczego.')
+	})
+
+	.post('/', (req, res) => {
+		res.send('Dodaj pojedynczego!')
+	})
+
+	.delete('/', (req, res) => {
+		res.send('Usuń pojedynczego!')
+	})
 
 module.exports = {
 	clientRouter,
