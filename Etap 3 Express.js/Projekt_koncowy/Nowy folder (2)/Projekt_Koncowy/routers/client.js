b85ee4dc6e937b54200fc58b934d25360b2709c1
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-11-16 13:18:40 +0100

    add

--- Etap 3 Express.js/Projekt_koncowy/Nowy folder (2)/Projekt_Koncowy/routers/client.js
+++ Etap 3 Express.js/Projekt_koncowy/Nowy folder (2)/Projekt_Koncowy/routers/client.js
@@ -9,17 +9,20 @@ clientRouter
       clients : DbClient.getAll()
     })
   })
+
   .get('/:id',(req, res) => {
     res.render('client/get-one',{
       client : DbClient.getOne(req.params.id)
     })
   })
+
   .post('/form/add',(req, res) => {
     const id = DbClient.create(req.body);
     res.render('client/added',{
       client : DbClient.getOne(id)
     })
   })
+
   .delete('/:id',(req, res) => {
     const name = DbClient.getOne(req.params.id).name;
     DbClient.delete(req.params.id);
@@ -27,15 +30,17 @@ clientRouter
       name
     })
   })
+
   .get('/form/add',(req, res) => {
     res.render('client/form_create')
   })
-  .get('/form/edit/:id',(req, res) => {
 
+  .get('/form/edit/:id',(req, res) => {
     res.render('client/edit-user',{
       client: DbClient.getOne(req.params.id)
     })
   })
+
   .put('/form/edit/:id',(req, res) => {
     DbClient.update(req.params.id,req.body)
     res.render('client/get-one',{
