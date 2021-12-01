641e5181c990a632d7d7a0edd6b80a29bc6bff5c
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-12-01 12:24:14 +0100

    Update calc.js

--- Etap 3 Express.js/Milena/routes/calc.js
+++ Etap 3 Express.js/Milena/routes/calc.js
@@ -2,14 +2,12 @@ const express = require('express');
 
 const calcRouter = express.Router();
 
-
-
 calcRouter
-    .post('/calc/check',(req,res)=>{
+    .post('/calc/check', (req, res) => {
         console.log(req.body);
-        res.json({ok: true})
-})
+        res.json({ ok: true })
+    })
 
-module.exports={
+module.exports = {
     calcRouter,
 }
