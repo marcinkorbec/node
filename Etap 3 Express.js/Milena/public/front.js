ed4bc0eeed070cbf511beec3e794dc469036cea3
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-12-01 12:25:37 +0100

    Update front.js

--- Etap 3 Express.js/Milena/public/front.js
+++ Etap 3 Express.js/Milena/public/front.js
@@ -1,18 +1,19 @@
-const numberA=Number(numberAInput.value);
-const numberB=Number(numberBInput.value)
-    console.log({numberA,numberB})
-   
-    const result= fetch('/calc/check', {
-        method: 'POST',
-        body: JSON.stringify({
-            numberA,
-            numberB,
-        }),
-        
-        headers: {
-            'Content-type':'application/json'
-        },
-    })
+const numberA = Number(numberAInput.value);
+const numberB = Number(numberBInput.value)
+console.log({ numberA, numberB })
 
-    const data=await res.json();
-    console.log(data);
+const result = fetch('/calc/check', {
+    method: 'POST',
+    body: JSON.stringify({
+        numberA,
+        numberB,
+    }),
+
+
+    headers: {
+        'Content-type': 'application/json'
+    },
+})
+
+const data = await res.json();
+console.log(data);
