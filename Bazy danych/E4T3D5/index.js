e9dcd4126fc5f83cac44b7c68a6a6ed8cabb6c3b
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-12-14 10:23:52 +0100

    Update index.js

--- Bazy danych/E4T3D5/index.js
+++ Bazy danych/E4T3D5/index.js
@@ -1,15 +1,17 @@
 const mysql = require('mysql2/promise');
 
+const pool = mysql.createPool({
+	host: 'localhost',
+	user: 'root',
+	database: 'megak_courses',
+	decimalNumbers: true,
+	namedPlaceholders: true,
+	bigNumberStrings: false,
+});
 
 (async () => {
-	const pool = mysql.createPool({
-		host: 'localhost',
-		user: 'root',
-		database: 'megak_courses',
-		decimalNumbers: true,
-		namedPlaceholders: true,
-		bigNumberStrings: false,
-	});
 
+	const [results] = await pool.execute('SELECT * FROM `courses`');
+	console.log(results)
 
 })();
