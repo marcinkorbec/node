2b986582bd799309d223e6ab603cc65ea329d779
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-12-23 20:20:52 +0100

    Update index.js

--- Bazy danych/E4T4D5/index.js
+++ Bazy danych/E4T4D5/index.js
@@ -5,9 +5,6 @@ const client = new MongoClient('mongodb://localhost:27017', {useNewUrlParser: tr
 (async() => {
 	await client.connect();
 	const db = client.db('megak_music');
-	const artist = {$ne: ''};
-	for await (const result of db.collection('songs').find({artist})) {
-		console.log(result);
-	};
+	
 	await client.close();
 })();
