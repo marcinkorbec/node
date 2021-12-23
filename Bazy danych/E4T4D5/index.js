8d4bba8f665de8a559b0648332e9c1c8ff894696
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-12-23 19:30:44 +0100

    Update index.js

--- Bazy danych/E4T4D5/index.js
+++ Bazy danych/E4T4D5/index.js
@@ -1,12 +1,21 @@
-const mongo = require('mongodb');
-const {ObjectId} = require("mongodb");
+const {MongoClient, ObjectId} = require("mongodb");
 
-const client = new mongo.MongoClient('mongodb://localhost:27017', {useNewUrlParser: true});
+const client = new MongoClient('mongodb://localhost:27017', {useNewUrlParser: true});
 
 (async() => {
 	await client.connect();
 	const db = client.db('megak_music');
-	const oneSong = await db.collection('songs').findOne({_id: mongo.ObjectId('61bcbc1b70408673381f8fa9')});
-	console.log(oneSong);
+	const result = await db.collection('songs').updateOne({
+		_id: ObjectId('61bcbc1b70408673381f8fa9')
+		}, {
+				$set: {
+					artist: 'Kaśka Sochacka',
+				},
+				$inc: {
+					length: 10,
+				}
+			},
+		);
+	console.log(result);
 	await client.close();
 })();
