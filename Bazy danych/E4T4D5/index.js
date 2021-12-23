9eb5fa5f0b69aa675afc0e82e2ede9bc5ddc37d2
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-12-23 20:40:31 +0100

    Update index.js

--- Bazy danych/E4T4D5/index.js
+++ Bazy danych/E4T4D5/index.js
@@ -7,5 +7,29 @@ const client = new MongoClient('mongodb://localhost:27017', {useNewUrlParser: tr
 	const db = client.db('megak_music2');
 	await db.createCollection('songs');
 
+	const songs = [
+		{
+			title: 'Długość dźwięku samotności',
+			artist: 'Artur Rojek',
+			length: 350,
+		},
+		{
+			title: 'Beksa',
+			artist: 'Artur Rojek',
+			length: 330,
+		},
+		{
+			title: 'Na cały świat',
+			artist: 'Sokół',
+			length: 380,
+		},
+		{
+			title: 'Szczęścia milimetr',
+			artist: 'O.S.T.R.',
+			length: 360,
+		}
+	];
+
+	await db.collection('songs').insertMany(songs);
 	await client.close();
 })();
