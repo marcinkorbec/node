73b27464e419fc247d0f83dd7975af98853c64bc
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-12-26 21:39:21 +0100

    Update index.js

--- Bazy danych/E4T5D2/index.js
+++ Bazy danych/E4T5D2/index.js
@@ -23,7 +23,7 @@ const {ObjectId} = require("mongodb");
 		// const todo = await TodoRepository.find('61c61d2682532f5af6aa7188');
 		// todo.title = 'Coś zwykłego'
 		// await TodoRepository.delete(todo);
-		console.log(await TodoRepository.find('61c61d2682532f5af6aa7188'));
+		// console.log(await TodoRepository.find('61c61d2682532f5af6aa7188'));
 
 	}
 	finally {
