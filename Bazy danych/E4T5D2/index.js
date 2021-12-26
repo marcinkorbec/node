9a5d58e61b43dd804429a2d829490b2cd47bfb25
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-12-26 21:41:09 +0100

    Update index.js

--- Bazy danych/E4T5D2/index.js
+++ Bazy danych/E4T5D2/index.js
@@ -7,7 +7,7 @@ const {ObjectId} = require("mongodb");
 	try {
 		DODAWANIE REKORDU
 		const todo = new TodoRecord({
-		// 	title: 'Skończyć 2 projekt MongoDB',
+			title: 'Skończyć 2 projekt MongoDB',
 		// 	_id: '61c61f67c82843560b6af652',
 		// });
 		// await TodoRepository.insert(todo);
