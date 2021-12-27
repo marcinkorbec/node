7ad970195f4bcca07db99a2e2653866e5f186458
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-12-27 17:57:21 +0100

    Update index.js

---
+++ Bazy danych/E4T5D4/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const methodOverride = require('method-override');
+const app = express();
+const hbs = require('express-handlebars');
+
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({
+    extended: true,
+}));
+app.use(express.static('public'));
+app.use(express.json()); //Content-type: application/json
+app.engine('.hbs', hbs.engine({
+    extname: '.hbs',
+    helpers: handlebarsHelpers,
+}));
+app.set('view engine', '.hbs');
+
+app.get('/', (req, res) => {
+    res.send(<h1>Hello World</h1>);
+});
+
+app.use(handleError);
+
+app.listen(3002, '0.0.0.0', () => {
+    console.log('Program działa na adresie http://localhost:3000');
+})
