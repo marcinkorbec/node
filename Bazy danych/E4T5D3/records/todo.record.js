982148d78d44e832b13a966ca06ec1e3672c68ac
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-12-27 13:49:22 +0100

    add

--- Bazy danych/E4T5D3/records/todo.record.js
+++ Bazy danych/E4T5D3/records/todo.record.js
@@ -1,4 +1,4 @@
-const {pool} = require("../utils/db");
+const {pool, todos} = require("../utils/db");
 const {v4: uuid} = require('uuid')
 const {ObjectId} = require("mongodb");
 
@@ -17,6 +17,45 @@ class TodoRecord {
 		if (this.title.length > 150) {
 			throw new Error('Tytuł Todosa nie powinien być dłuższy niż 150 znaków.');
 		}
+	}
+
+	static _checkRecord(record) {
+		if (!(record instanceof TodoRecord)) {
+			throw new Error('Tworzony obiekt nie jest instancją klasy TodoRecord.')
+		}
+	}
+
+	static async insert(record) {
+		TodoRepository._checkRecord(record);
+		const {insertedId} = await todos.insertOne(record);
+		record._id = insertedId;
+		console.log(insertedId)
+	}
+
+	static async delete(record) {
+		TodoRepository._checkRecord(record);
+
+		await todos.deleteOne({
+			_id: record._id,
+		})
+	}
+
+	static async findAll() {
+		return (await todos.find()).toArray();
+	}
+
+	static async find(id) {
+		const item = await todos.findOne({_id: ObjectId(String(id))});
+		return item === null ? null: new TodoRecord(item);
+	}
+
+	static async update(record) {
+		TodoRepository._checkRecord(record);
+		await todos.replaceOne({
+			_id: record._id,
+		}, {
+			title: String(record.title),
+		});
 	}
 }
 
