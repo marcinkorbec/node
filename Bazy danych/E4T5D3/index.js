f649feff0ab6e3c060a3f5ec7aeab44852bc8ffa
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-12-27 15:31:38 +0100

    Update index.js

--- Bazy danych/E4T5D3/index.js
+++ Bazy danych/E4T5D3/index.js
@@ -22,8 +22,10 @@ const {ObjectId} = require("mongodb");
 
 		//UPDATOWANIE Z KURSORA
 		for await (const todo of await TodoRecord.findAllWithCursor()) {
-			
-			console.log(new TodoRecord(todo));
+			const record = new TodoRecord(todo)
+			record.title += ' [updated]';
+			await record.update();
+			console.log(await TodoRecord.findAll());
 		}
 
 
