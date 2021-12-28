76ae5cdd65cf318470a5902fb856d5a38d89d604
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-12-28 10:23:18 +0100

    add

---
+++ Bazy danych/E4T5D4/utils/db.js
@@ -0,0 +1,12 @@
+const {createPool} = require("mysql2/promise");
+const pool = createPool({
+	host: 'localhost',
+	user: 'root',
+	database: 'megak_santa_gifts',
+	namedPlaceholders: true,
+	decimalNumbers: true,
+})
+
+module.exports = {
+	pool,
+}
