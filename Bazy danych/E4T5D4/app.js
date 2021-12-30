de5143958ad09d01d430c580a16da94d9e2af4b6
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-12-30 13:52:22 +0100

    Update app.js

--- Bazy danych/E4T5D4/app.js
+++ Bazy danych/E4T5D4/app.js
@@ -1,5 +1,12 @@
-const { mkdir } = require('fs').promises;
-
+const { rename } = require('fs').promises;
 (async () => {
-    await mkdir('./megaK');
+    try {
+        await rename('./megaK/txt/testZmiana.txt', './testPrzeniesienie.txt')
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            console.log('Given file name does not exist.');
+        } else {
+            console.log('Oh no!', err);
+        }
+    }
 })();
