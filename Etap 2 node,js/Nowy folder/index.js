c07d616ebbc2eefd7c342974eac47b326ea506b8
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-12-30 17:20:11 +0100

    Update index.js

--- Etap 2 node,js/Nowy folder/index.js
+++ Etap 2 node,js/Nowy folder/index.js
@@ -1,22 +1,40 @@
-const { watch } = require('chokidar');
+// const { watch } = require('chokidar');
 
-watch('.')
-    .on('add', path => console.log(`File ${path} has been added`))
-    .on('change', path => console.log(`File ${path} has been changed`))
-    .on('unlink', path => console.log(`File ${path} has been removed`))
-    .on('addDir', path => console.log(`Directory ${path} has been added`))
-    .on('unlinkDir', path => console.log(`Directory ${path}has been removed`))
-    .on('error', error => console.log(`Watcher error ${error}`))
-    .on('ready', () => console.log('Initial scan complete,Ready for changes'));
+// watch('.')
+//     .on('add', path => console.log(`File ${path} has been added`))
+//     .on('change', path => console.log(`File ${path} has been changed`))
+//     .on('unlink', path => console.log(`File ${path} has been removed`))
+//     .on('addDir', path => console.log(`Directory ${path} has been added`))
+//     .on('unlinkDir', path => console.log(`Directory ${path}has been removed`))
+//     .on('error', error => console.log(`Watcher error ${error}`))
+//     .on('ready', () => console.log('Initial scan complete,Ready for changes'));
 
-watch('.', {
-    usePolling: true,
-    awaitWriteFinish: true,
-    ignoreInitial: true,
-})
+// watch('.', {
+//     usePolling: true,
+//     awaitWriteFinish: true,
+//     ignoreInitial: true,
+// })
+
+// const { exec } = require('child_process');
+// const cp = exec('dir');
+// cp.on('close', () => {
+//     console.log('Finished!');
+// })
+
+// const { exec } = require('child_process');
+// const cp = exec('dir', {
+//     env: {
+//         PATH: 'c:\\Users\\Marcin\\Desktop\\MegaKurs\\Node\\'
+//     }
+// });
+// cp.on('close', () => {
+//     console.log('Finished!');
+// })
 
 const { exec } = require('child_process');
-const cp = exec('dir');
+const cp = exec('dir', {
+    cwd: 'c:\\Users\\szymo\\Desktop\\MegaKurs\\Node\\'
+});
 cp.on('close', () => {
     console.log('Finished!');
 })
