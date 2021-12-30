96d4184bea14e5035af7cd61f421fdb810c12d23
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-12-30 17:16:44 +0100

    add

--- Etap 2 node,js/Nowy folder/index.js
+++ Etap 2 node,js/Nowy folder/index.js
@@ -1,5 +1,22 @@
-const { normalize, join } = require('path');
-const userPath = normalize(join(__dirname, process.argv[2]));
-console.log(userPath);
+const { watch } = require('chokidar');
 
+watch('.')
+    .on('add', path => console.log(`File ${path} has been added`))
+    .on('change', path => console.log(`File ${path} has been changed`))
+    .on('unlink', path => console.log(`File ${path} has been removed`))
+    .on('addDir', path => console.log(`Directory ${path} has been added`))
+    .on('unlinkDir', path => console.log(`Directory ${path}has been removed`))
+    .on('error', error => console.log(`Watcher error ${error}`))
+    .on('ready', () => console.log('Initial scan complete,Ready for changes'));
 
+watch('.', {
+    usePolling: true,
+    awaitWriteFinish: true,
+    ignoreInitial: true,
+})
+
+const { exec } = require('child_process');
+const cp = exec('dir');
+cp.on('close', () => {
+    console.log('Finished!');
+})
