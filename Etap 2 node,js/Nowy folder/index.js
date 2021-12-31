457536980544a292a4452e29cc554562a8504018
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-12-31 19:48:05 +0100

    add

--- Etap 2 node,js/Nowy folder/index.js
+++ Etap 2 node,js/Nowy folder/index.js
@@ -39,22 +39,24 @@
 //     console.log('Finished!');
 // })
 
-// const { exec } = require('child_process');
 
-// const cp = exec('dir', {
-//     cwd: 'c:\\Users\\szymo\\Desktop\\MegaKurs\\Node\\'
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec);
+
+// (async () => {
+// const data = await exec('ping 8.8.8.8');
+// console.log(data.stdout);
+// })();
+
+// const { pbkdf2 } = require('crypto');
+// const SALT = 'SDA;FJ4K008191284310923&(!    &^ TFJ: D* Y#j; lrkfaj; lkfj; dlaisdfjm; asiojea; l83uvnhao * (YH & (&@^* !& YF *& ';
+// const GOOD_PASSWORD = '1dc6cdd1e7945e87dcdbc7fea8bbebc983a3f105f1157ee3c6aba9c403a7d62fd9acdb8c86552ff8eea0a26e3edeed376cc381d118422bc9a96d0d118267f35c';
+// const pwd = process.argv[2];
+// pbkdf2(pwd, SALT, 100000, 64, 'sha512', (err, derivedKey) => {
+//     if (err) {
+//         console.error('Error', err);
+//     } else if (derivedKey.toString('hex') === GOOD_PASSWORD) {
+//         console.log('Logged in.');
+//     }
 // });
-// cp.on('close', () => {
-//     console.log('Finished!');
-// })
 
-
-const { exec } = require('child_process');
-const cp = exec('dir', {
-    env: {
-        PATH: 'c:\\Users\\szymo\\Desktop\\MegaKurs\\Node\\'
-    }
-});
-cp.on('close', () => {
-    console.log('Finished!');
-})
