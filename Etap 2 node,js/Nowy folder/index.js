a6c67ada3f20ca942cbb8d45d817c3eadcb38cf1
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2021-12-31 19:48:33 +0100

    Update index.js

--- Etap 2 node,js/Nowy folder/index.js
+++ Etap 2 node,js/Nowy folder/index.js
@@ -48,15 +48,15 @@ const exec = promisify(require('child_process').exec);
 // console.log(data.stdout);
 // })();
 
-// const { pbkdf2 } = require('crypto');
-// const SALT = 'SDA;FJ4K008191284310923&(!    &^ TFJ: D* Y#j; lrkfaj; lkfj; dlaisdfjm; asiojea; l83uvnhao * (YH & (&@^* !& YF *& ';
-// const GOOD_PASSWORD = '1dc6cdd1e7945e87dcdbc7fea8bbebc983a3f105f1157ee3c6aba9c403a7d62fd9acdb8c86552ff8eea0a26e3edeed376cc381d118422bc9a96d0d118267f35c';
-// const pwd = process.argv[2];
-// pbkdf2(pwd, SALT, 100000, 64, 'sha512', (err, derivedKey) => {
-//     if (err) {
-//         console.error('Error', err);
-//     } else if (derivedKey.toString('hex') === GOOD_PASSWORD) {
-//         console.log('Logged in.');
-//     }
-// });
+const { pbkdf2 } = require('crypto');
+const SALT = 'SDA;FJ4K008191284310923&(!    &^ TFJ: D* Y#j; lrkfaj; lkfj; dlaisdfjm; asiojea; l83uvnhao * (YH & (&@^* !& YF *& ';
+const GOOD_PASSWORD = '1dc6cdd1e7945e87dcdbc7fea8bbebc983a3f105f1157ee3c6aba9c403a7d62fd9acdb8c86552ff8eea0a26e3edeed376cc381d118422bc9a96d0d118267f35c';
+const pwd = process.argv[2];
+pbkdf2(pwd, SALT, 100000, 64, 'sha512', (err, derivedKey) => {
+    if (err) {
+        console.error('Error', err);
+    } else if (derivedKey.toString('hex') === GOOD_PASSWORD) {
+        console.log('Logged in.');
+    }
+});
 
