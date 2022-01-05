eb698d18a84a306d75b87ea8e5c1fda97648f043
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-01-05 12:03:54 +0100

    Update news.js

---
+++ Etap 3 Express.js/projekt pug/models/news.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const newsSchema = new Schema({
+	title: { type:String, required: true},
+	description: {type:String, required: true},
+	date: {
+		type: Date,
+		default: Date.now
+	},
+})
+
+module.exports = mongoose.model('News', newsSchema);
