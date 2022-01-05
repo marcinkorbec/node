9501e89b7b25619214c3954c8d95c99856f7988c
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-01-05 12:36:56 +0100

    add

--- Etap 3 Express.js/projekt pug/routes/admin.js
+++ Etap 3 Express.js/projekt pug/routes/admin.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const News = require('../models/news')
 
 router.all('*', (req, res, next) => {
 	if (!req.session.admin) {
@@ -11,8 +12,20 @@ router.all('*', (req, res, next) => {
 
 /* GET home page. */
 router.get('/', (req, res ) => {
+	const newsData = new News({
+		title: 'Tytuł testowy',
+		description: 'Opis'
+	})
+
+	newsData.save((error) => {
+		console.log(error);
+	});
 	console.log(req.session.admin);
-	res.render('admin', { title: 'Admin' });
+	res.render('admin/index', { title: 'Admin' });
 });
+
+router.get('/news/add', (req, res) => {
+
+})
 
 module.exports = router;
