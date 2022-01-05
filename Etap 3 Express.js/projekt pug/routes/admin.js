e75473f7de5767c060b4837357ca899cc3eded40
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-01-05 12:53:58 +0100

    add

--- Etap 3 Express.js/projekt pug/routes/admin.js
+++ Etap 3 Express.js/projekt pug/routes/admin.js
@@ -2,30 +2,31 @@ const express = require('express');
 const router = express.Router();
 const News = require('../models/news')
 
-router.all('*', (req, res, next) => {
-	if (!req.session.admin) {
-		res.redirect('/login')
-	}
+router
+	.all('*', (req, res, next) => {
+		if (!req.session.admin) {
+			res.redirect('/login')
+		}
 
-	next();
-})
-
-/* GET home page. */
-router.get('/', (req, res ) => {
-	const newsData = new News({
-		title: 'Tytuł testowy',
-		description: 'Opis'
+		next();
 	})
 
-	newsData.save((error) => {
-		console.log(error);
-	});
-	console.log(req.session.admin);
-	res.render('admin/index', { title: 'Admin' });
-});
+	/* GET home page. */
+	.get('/', (req, res) => {
+		const newsData = new News({
+			title: 'Tytuł testowy',
+			description: 'Opis'
+		})
 
-router.get('/news/add', (req, res) => {
+		newsData.save((error) => {
+			console.log(error);
+		});
+		console.log(req.session.admin);
+		res.render('admin/index', {title: 'Admin'});
+	})
 
-})
+	.get('/news/add', (req, res) => {
+		res.render('admin/news-form', {title: 'Dodaj news'});
+	})
 
 module.exports = router;
