aa0a0107d7eab6d6aaa273b54d24cf07c855f78c
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-01-06 22:29:25 +0100

    Update quiz.js

--- Etap 3 Express.js/projekt pug/models/quiz.js
+++ Etap 3 Express.js/projekt pug/models/quiz.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 /* CREATE Scheme for objects to database. */
 const quizSchema = new Schema({
 	title: { type:String, required: true},
-	votes: {type:Number, required: true, default: 0},
+	votes: {type:Number, required: true, default: 0},//
 })
 
 module.exports = mongoose.model('Quiz', quizSchema);
