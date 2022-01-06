bbc0a7469ef79098c1b73fbcfd2318f40de596b6
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-01-06 20:11:38 +0100

    add

--- Etap 3 Express.js/projekt pug/routes/news.js
+++ Etap 3 Express.js/projekt pug/routes/news.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const News = require('../models/news')
 
 /* GET home page. */
 router.get('/', (req, res ) => {
-	res.render('news', { title: 'News' });
+	const findNews = News.find();
+
+	findNews.exec((err, data) => {
+		console.log(data);
+		res.render('news', { title: 'News', data});
+	})
+
 });
 
 module.exports = router;
