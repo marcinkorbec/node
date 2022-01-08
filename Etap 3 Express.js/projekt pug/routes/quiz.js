58ca540780f7399691bb3063628048a013794226
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-01-08 14:30:39 +0100

    add

--- Etap 3 Express.js/projekt pug/routes/quiz.js
+++ Etap 3 Express.js/projekt pug/routes/quiz.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const Quiz = require('../models/quiz')
+const Quiz = require('../models/quiz');
+
 /* GET home page. */
-router.get('/', (req, res ) => {
-	res.render('quiz', { title: 'Quiz' });
+router.get('/', (req, res) => {
+	Quiz.find({}, (error, databaseSeasons) => {
+		console.log(databaseSeasons)
+		res.render('quiz', {title: 'Quiz', databaseSeasons});
+	})
 });
 
 module.exports = router;
