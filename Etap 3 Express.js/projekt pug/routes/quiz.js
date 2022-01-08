6123c3cfacd1db0df919120a53d62f87ed587fb6
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-01-08 14:04:41 +0100

    Update quiz.js

--- Etap 3 Express.js/projekt pug/routes/quiz.js
+++ Etap 3 Express.js/projekt pug/routes/quiz.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const Quiz = require('../models/quiz')
 /* GET home page. */
 router.get('/', (req, res ) => {
-	new Quiz({ title: 'Pytanie 1', vote: 0 })
 	res.render('quiz', { title: 'Quiz' });
 });
 
