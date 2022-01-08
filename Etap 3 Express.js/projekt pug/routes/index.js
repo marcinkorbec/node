feb7aa4636335410b6547bca8ec8a77a4db80915
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-01-08 21:05:37 +0100

    add

--- Etap 3 Express.js/projekt pug/routes/index.js
+++ Etap 3 Express.js/projekt pug/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res ) => {
-  res.render('index', { title: 'Marcin' });
+  res.render('admin', { title: 'Marcin'});
 });
 
 module.exports = router;
