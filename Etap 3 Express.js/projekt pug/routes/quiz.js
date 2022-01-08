3607a2ff0d91a1b289bcc1db1e751e6bee8f58d0
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-01-08 15:22:04 +0100

    Update quiz.js

--- Etap 3 Express.js/projekt pug/routes/quiz.js
+++ Etap 3 Express.js/projekt pug/routes/quiz.js
@@ -2,12 +2,22 @@ const express = require('express');
 const router = express.Router();
 const Quiz = require('../models/quiz');
 
-/* GET home page. */
-router.get('/', (req, res) => {
-	Quiz.find({}, (error, databaseSeasons) => {
-		console.log(databaseSeasons);
-		res.render('quiz', {title: 'Quiz', databaseSeasons});
+	/* GET quiz page. */
+router
+	.get('/', (req, res) => {
+		Quiz.find({}, (error, databaseSeasons) => {
+			console.log(databaseSeasons);
+			res.render('quiz', {title: 'Quiz', databaseSeasons});
+		})
 	})
-});
+
+	/* SEND votes to database Seasons. */
+	.post('/', (req, res) => {
+		const dataSend = req.bosy.quiz;
+
+		Quiz.find({}, (error, data) => {
+			res.render('quiz', {title: 'Quiz', data});
+		})
+	})
 
 module.exports = router;
