d0619da2a80a8374b8ff4f399d03e0edc31e5e23
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-01-12 15:20:03 +0100

    Update index.ts

--- Etap 5 Typescript/E5T1D1/index.ts
+++ Etap 5 Typescript/E5T1D1/index.ts
@@ -1,19 +1,21 @@
-function sum(a: number, b: number) {
-    return a + b;
-}
+// function sum(a: number, b: number) {
+//     return a + b;
+// }
 
-const input = document.querySelector('input');
-if (input) {
-    const val = parseInt(input.value);
+// const input = document.querySelector('input');
+// if (input) {
+//     const val = parseInt(input.value);
 
-    console.log(sum(2, 3));
-    console.log(sum(2, val));
+//     console.log(sum(2, 3));
+//     console.log(sum(2, val));
 
 
-}
+// }
 
-const add = (a: any, b: any): number | string => {
-    return a + b;
-}
-console.log(add(2, 3));
-console.log(add('Hello', ' World!'));
+// const add = (a: any, b: any): number | string => {
+//     return a + b;
+// }
+// console.log(add(2, 3));
+// console.log(add('Hello', ' World!'));
+
+const bool 
