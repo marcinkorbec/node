8613946cc06dd4c54e9ea702cc21d3f147e207f9
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-01-14 22:29:44 +0100

    add

--- Etap 5 Typescript/E1T1D5/index.ts
+++ Etap 5 Typescript/E1T1D5/index.ts
@@ -1,103 +1,16 @@
-// function sum(a: number, b: number) {
-//     return a + b;
-// }
-
-// const input = document.querySelector('input');
-// if (input) {
-//     const val = parseInt(input.value);
-
-//     console.log(sum(2, 3));
-//     console.log(sum(2, val));
-
-
-// }
-
-// const add = (a: any, b: any): number | string => {
-//     return a + b;
-// }
-// console.log(add(2, 3));
-// console.log(add('Hello', ' World!'));
-
-// function date(person: Person) {
-//     let today = new Date();
-//     let yr = today.getFullYear();
-//     console.log(`Date of birth ${this.name} is ${yr}`);
-// }
-
-// function greet(person: Person) {
-//     console.log(`Hello, ${person.name}`);
-// }
-
-
-interface Person {
+interface Human {
     name: string;
-    yearOfBirth: number;
-    isDeveloper: boolean;
-    greet(): string;
-    age(): number;
+    age: number;
+    achievements?: string;
 }
 
 
-
-const personA: Person = {
-    name: 'Bartek',
-    yearOfBirth: 1970,
-    isDeveloper: true,
-    greet() {
-        return this.name
-    },
-    age() {
-        const today: number = new Date().getFullYear();
-        const age: number = today - this.yearOfBirth;
-        return age;
+class Person implements Human {
+    name: string;
+    age: number;
+    
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
     }
-};
-
-console.log(`Hello ${personA.greet()}, ${personA.name} is ${personA.age()} years old`);
-
-
-
-// const personB: Person = {
-//     name: 'Kuba',
-//     age: 122,
-//     isDeveloper: true,
-//     greet: function (): void {
-//         console.log(`Hello ${this.name}`);
-//     },
-//     dateOfBirth() {
-//         let today: Date = new Date();
-//         let yr: number = today.getFullYear();
-//         console.log(`Date of birth ${this.name} is ${yr}`);
-//     }
-// }
-
-// // greet(personB);
-
-// greet({
-//     name: 'Testerka',
-//     age: 34,
-//     isDeveloper: true,
-//     greet() {
-//         console.log(`Hello ${this.name}`);
-//     },
-//     dateOfBirth() {
-//         let today: Date = new Date();
-//         let yr: number = today.getFullYear();
-//         console.log(`Date of birth ${this.name} is ${yr}`);
-//     }
-// });
-
-// const personC: Person = {
-//     name: 'Marta',
-//     age: 23,
-//     isDeveloper: true,
-//     greet() {
-//         console.log(`Hello ${this.name}`);
-//     },
-//     dateOfBirth() {
-//         let today: Date = new Date();
-//         let yr: number = today.getFullYear();
-//         console.log(`Date of birth ${this.name} is ${yr}`);
-//     }
-// }
-
+}
