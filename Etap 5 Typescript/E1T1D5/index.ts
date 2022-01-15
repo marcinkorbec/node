523f964ac020086a887d26c3897ab30ca6368fb9
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-01-15 19:49:58 +0100

    add

--- Etap 5 Typescript/E1T1D5/index.ts
+++ Etap 5 Typescript/E1T1D5/index.ts
@@ -5,15 +5,23 @@ interface Human {
     achievements?: string;
 }
 
+interface HistoryClass {
+    history: string[]; //tablica stringów
+    showHistory(): void;
+}
 
-class Person implements Human {
+class Person implements Human, HistoryClass {
     name: string;
     age: number;
-    surname: string;
+    surname: string = '';
+    history: string[] = [];
 
     constructor(name: string, age: number) {
         this.name = name;
         this.age = age;
+    }
+    showHistory(): void {
+        console.log(this.history);
     }
 }
 
