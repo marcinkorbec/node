4d2be16555b503060cf859cf704526f1eddd363d
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-01-15 15:52:01 +0100

    add

--- Etap 5 Typescript/E1T1D5/index.ts
+++ Etap 5 Typescript/E1T1D5/index.ts
@@ -1,5 +1,6 @@
 interface Human {
     name: string;
+    surname: string;
     age: number;
     achievements?: string;
 }
@@ -8,9 +9,13 @@ interface Human {
 class Person implements Human {
     name: string;
     age: number;
+    surname: string;
 
     constructor(name: string, age: number) {
         this.name = name;
         this.age = age;
     }
 }
+
+const kuba = new Person('Kuba', 27);
+console.log(kuba);
