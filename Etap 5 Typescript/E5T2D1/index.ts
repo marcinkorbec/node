63155937c8d1bcba23c9eac5acc727de2f6f6a7a
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-01-20 16:11:04 +0100

    add

--- Etap 5 Typescript/E5T2D1/index.ts
+++ Etap 5 Typescript/E5T2D1/index.ts
@@ -24,3 +24,50 @@
 
 /* ZADANIE 2 */
 
+interface Person {
+    name: string;
+    points: number;
+}
+
+
+const data:  Person[] = [
+    {
+        name: 'Anna',
+        points: 1000,
+    },
+    {
+        name: 'Krzysztof',
+        points: 500,
+    },
+    {
+        name: 'Ola',
+        points: 0,
+    },
+    {
+        name: 'Marek',
+        points: 0,
+    },
+];
+
+function incPoints(obj: Person) :void {
+    obj.points++;
+}
+
+function totalPoints(ar:  Person[]) : number {
+   return ar.reduce((prev, curr) => prev + curr.points, 0);
+}
+
+function personWithMostPoints(ar:  Person[]): Person {
+    return ar.reduce((prev, curr) => {
+        if(prev.points < curr.points) {
+            return curr;
+        } else {
+            return prev;
+        }
+    });
+}
+
+console.log(totalPoints(data))
+incPoints(data[1]);
+console.log(totalPoints(data));
+console.log(personWithMostPoints(data))
