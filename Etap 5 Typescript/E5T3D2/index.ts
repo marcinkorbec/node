32c800aeccb207f97c00d20fd9fe64db97693a0f
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-01-25 16:39:13 +0100

    Update index.ts

--- Etap 5 Typescript/E5T3D2/index.ts
+++ Etap 5 Typescript/E5T3D2/index.ts
@@ -5,10 +5,16 @@ interface SingleToDo {
     completed: boolean;
 }
 
+interface SingleToDoCreate {
+    userId: number;
+    title: string;
+    completed: boolean;
+}
+
 class TodoApi {
     private readonly url = 'https://jsonplaceholder.typicode.com/todos/';
 
-   async get (id: number): Promise<SingleToDo> {
+    async get(id: number): Promise<SingleToDo> {
         const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
         return await response.json();
     }
@@ -17,14 +23,34 @@ class TodoApi {
         const response = await fetch(`https://jsonplaceholder.typicode.com/todos/`);
         return await response.json();
     }
+
+    async create(task: SingleToDoCreate):Promise<SingleToDo> {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/todos/`, {
+            method: 'POST',
+            body: JSON.stringify(task),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        });
+        return await response.json();
+    }
 }
 
-(async ()=> {
-    const todo = new TodoApi();
-    console.log(await todo.get(1));
-})();
+// (async () => {
+//     const todo = new TodoApi();
+//     console.log(await todo.get(1));
+// })();
+//
+// (async () => {
+//     const todo = new TodoApi();
+//     console.log(await todo.list());
+// })();
 
-(async ()=> {
+(async () => {
     const todo = new TodoApi();
-    console.log(await todo.list());
+    console.log(await todo.create({
+        title: 'foo',
+        userId: 1,
+        completed: false,
+    }));
 })();
