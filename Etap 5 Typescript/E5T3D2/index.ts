3fad83136a81ef26d40654a5654033099eebd01f
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-01-25 19:57:21 +0100

    Update index.ts

--- Etap 5 Typescript/E5T3D2/index.ts
+++ Etap 5 Typescript/E5T3D2/index.ts
@@ -1,41 +1,41 @@
-interface SingleToDo {
-    userId: number;
-    id: number;
-    title: string;
-    completed: boolean;
-}
-
-interface SingleToDoCreate {
-    userId: number;
-    title: string;
-    completed: boolean;
-}
-
-class TodoApi {
-    private readonly url = 'https://jsonplaceholder.typicode.com/todos/';
-
-    async get(id: number): Promise<SingleToDo> {
-        const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
-        return await response.json();
-    }
-
-    async list(): Promise<SingleToDo[]> {
-        const response = await fetch(`https://jsonplaceholder.typicode.com/todos/`);
-        return await response.json();
-    }
-
-    async create(task: SingleToDoCreate):Promise<SingleToDo> {
-        const response = await fetch(`https://jsonplaceholder.typicode.com/todos/`, {
-            method: 'POST',
-            body: JSON.stringify(task),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            },
-        });
-        return await response.json();
-    }
-}
-
+// interface SingleToDo {
+//     userId: number;
+//     id: number;
+//     title: string;
+//     completed: boolean;
+// }
+//
+// interface SingleToDoCreate {
+//     userId: number;
+//     title: string;
+//     completed: boolean;
+// }
+//
+// class TodoApi {
+//     private readonly url = 'https://jsonplaceholder.typicode.com/todos/';
+//
+//     async get(id: number): Promise<SingleToDo> {
+//         const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+//         return await response.json();
+//     }
+//
+//     async list(): Promise<SingleToDo[]> {
+//         const response = await fetch(`https://jsonplaceholder.typicode.com/todos/`);
+//         return await response.json();
+//     }
+//
+//     async create(task: SingleToDoCreate):Promise<SingleToDo> {
+//         const response = await fetch(`https://jsonplaceholder.typicode.com/todos/`, {
+//             method: 'POST',
+//             body: JSON.stringify(task),
+//             headers: {
+//                 'Content-type': 'application/json; charset=UTF-8',
+//             },
+//         });
+//         return await response.json();
+//     }
+// }
+//
 // (async () => {
 //     const todo = new TodoApi();
 //     console.log(await todo.get(1));
@@ -45,12 +45,24 @@ class TodoApi {
 //     const todo = new TodoApi();
 //     console.log(await todo.list());
 // })();
+//
+// (async () => {
+//     const todo = new TodoApi();
+//     console.log(await todo.create({
+//         title: 'foo',
+//         userId: 1,
+//         completed: false,
+//     }));
+// })();
 
-(async () => {
-    const todo = new TodoApi();
-    console.log(await todo.create({
-        title: 'foo',
-        userId: 1,
-        completed: false,
-    }));
-})();
+enum ObjectType {
+    Women,
+    Men,
+    Xyz ,
+}
+
+const allPossibilities = Object
+    .keys(ObjectType)
+    .filter((key => Number.isNaN(Number(key))));
+
+console.log(allPossibilities)
