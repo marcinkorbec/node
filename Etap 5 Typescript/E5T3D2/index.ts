4890f9050549b7abf8064c17961e5f838b9395db
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-01-26 10:05:17 +0100

    add

--- Etap 5 Typescript/E5T3D2/index.ts
+++ Etap 5 Typescript/E5T3D2/index.ts
@@ -1,40 +1,5 @@
-// interface SingleToDo {
-//     userId: number;
-//     id: number;
-//     title: string;
-//     completed: boolean;
-// }
-//
-// interface SingleToDoCreate {
-//     userId: number;
-//     title: string;
-//     completed: boolean;
-// }
-//
-// class TodoApi {
-//     private readonly url = 'https://jsonplaceholder.typicode.com/todos/';
-//
-//     async get(id: number): Promise<SingleToDo> {
-//         const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
-//         return await response.json();
-//     }
-//
-//     async list(): Promise<SingleToDo[]> {
-//         const response = await fetch(`https://jsonplaceholder.typicode.com/todos/`);
-//         return await response.json();
-//     }
-//
-//     async create(task: SingleToDoCreate):Promise<SingleToDo> {
-//         const response = await fetch(`https://jsonplaceholder.typicode.com/todos/`, {
-//             method: 'POST',
-//             body: JSON.stringify(task),
-//             headers: {
-//                 'Content-type': 'application/json; charset=UTF-8',
-//             },
-//         });
-//         return await response.json();
-//     }
-// }
+
+
 //
 // (async () => {
 //     const todo = new TodoApi();
--- Etap 5 Typescript/E5T3D2/types/todo.ts
+++ Etap 5 Typescript/E5T3D2/types/todo.ts
@@ -1,6 +1,17 @@
-export interface Todo {
+interface SingleToDo {
     userId: number;
     id: number;
     title: string;
     completed: boolean;
+}
+
+interface SingleToDoCreate {
+    userId: number;
+    title: string;
+    completed: boolean;
+}
+
+export {
+    SingleToDo,
+    SingleToDoCreate,
 }
