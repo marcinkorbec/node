0bf12873b3f571725e9d598bd039b721a0800cb7
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-01-27 19:21:44 +0100

    add

--- Etap 5 Typescript/E5T3D2/index.ts
+++ Etap 5 Typescript/E5T3D2/index.ts
@@ -25,6 +25,41 @@
 //
 // console.log(Gender[Gender.Women]);
 
-import {addDays} from "date-fns";
 
-console.log(addDays(new Date(), 30));
+class Test {
+    push() {
+        console.log('Testing push...');
+        const ar = [];
+        const start = + new Date();
+        for (let i=0; i<100000;  i++) {
+            ar.push(i);
+        }
+        const end = + new Date();
+        console.log(`Zajęło to ${end - start} ms.`)
+    }
+
+    destructuring() {
+        console.log('Testing destructuring...');
+        let ar: number[] = [];
+        const start = + new Date();
+        for (let i=0; i<100000;  i++) {
+            ar = [...ar, i];
+        }
+        const end = + new Date();
+        console.log(`Zajęło to ${end - start} ms.`)
+    }
+
+    last() {
+        console.log('Testing last...');
+        const ar: number[] = [];
+        const start = + new Date();
+        for (let i=0; i<100000;  i++) {
+            ar[ar.length] = i;
+        }
+        const end = + new Date();
+        console.log(`Zajęło to ${end - start} ms.`)
+    }
+}
+
+const test = new Test();
+test.push();
