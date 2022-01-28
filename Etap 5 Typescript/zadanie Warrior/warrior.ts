db0f6416abec1ebb3b569ac8fb91b646c02aea66
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-01-28 18:49:46 +0100

    add

---
+++ Etap 5 Typescript/zadanie Warrior/warrior.ts
@@ -0,0 +1,38 @@
+interface Warriors {
+    name: string,
+    hitPoints: number,
+    hp: number,
+}
+
+export class Warrior {
+    name: string
+    hitPoints: number
+    hp: number
+
+     constructor(name: string, hitPoints: number, hp:number) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.hp = hp;
+    }
+
+    setHp(hp: number): void {
+        this.hp = hp;
+    }
+
+    getHp(): number {
+        return this.hp;
+    }
+
+    getHitPoints(): number {
+        return this.hitPoints;
+    }
+
+    getName(): string {
+        return this.name;
+    }
+
+    levelUp(): void {
+        this.hitPoints *= 1.1;
+        this.hp *= 1.1;
+    }
+}
