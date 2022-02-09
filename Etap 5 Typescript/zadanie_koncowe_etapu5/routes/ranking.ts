572511405ebd6c34d4b4b627ffe9541a411c40d8
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-02-09 16:24:30 +0100

    add

--- Etap 5 Typescript/zadanie_koncowe_etapu5/routes/ranking.ts
+++ Etap 5 Typescript/zadanie_koncowe_etapu5/routes/ranking.ts
@@ -1,1 +1,10 @@
-// ranking
+// ranking lista najlepszych wojowników
+
+import {Router} from "express";
+
+const ranking = Router();
+
+ranking
+    .get('/ranking', (req, res):void => {
+        res.render('views/ranking.hbs')
+    })
