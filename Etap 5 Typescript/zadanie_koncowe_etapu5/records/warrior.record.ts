e8b03a5bcb546042ea6e8e10fbc33d37f7772a65
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-02-11 20:35:05 +0100

    Update warrior.record.ts

--- Etap 5 Typescript/zadanie_koncowe_etapu5/records/warrior.record.ts
+++ Etap 5 Typescript/zadanie_koncowe_etapu5/records/warrior.record.ts
@@ -17,11 +17,39 @@ export class WarriorRecord {
 
         if (sum !== 10) {
             throw new ValidationError(`Suma wszystkich statystyk musi wynosić 10. Aktualnie jest to ${sum}.`);
-        }
+        };
 
         if (name.length < 3 && name.length > 40) {
-            throw new ValidationError(`Nick powinien być dłuższy niż 3 znaki`);
-        }
+            throw new ValidationError(`Nick powinien być dłuższy niż 3 znakii krótszy niż 40. Aktualnie jest to ${name.length}`);
+        };
+
+        this.id = id;
+        this.name = name;
+        this. strong = strong;
+        this.defense = defense;
+        this.resilience = resilience;
+        this.agility = agility;
+        this.wins = wins;
+    }
+
+    async insert() {
+
+    }
+
+    async update() {
+
+    }
+
+    static async getOne(id: string) {
+
+    }
+
+    static async listAll() {
+
+    }
+
+    static async topList(topCount: number) {
+
     }
 }
 
