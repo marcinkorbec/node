3c0786e6c937e532a11981bab1ae0adedddd03cf
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-02-11 19:43:18 +0100

    add

---
+++ Etap 5 Typescript/zadanie_koncowe_etapu5/records/warrior.record.ts
@@ -0,0 +1,21 @@
+export class WarriorRecord {
+    public id?: string;
+    public readonly name: string;
+    public readonly strong: number;
+    public readonly defense: number;
+    public readonly resilience: number;
+    public readonly agility: number;
+    public readonly wins?: number;
+
+    constructor(obj: WarriorRecord) {
+        const {id, name, strong, defense, resilience, wins, agility} = obj;
+
+        const sum = [strong, defense, resilience, agility].reduce((previousValue, currentValue) =>
+            previousValue + currentValue, 0);
+
+        if ( sum !== 10 {
+            throw new Error()
+        })
+    }
+}
+
