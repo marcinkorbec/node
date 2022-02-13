8465b3c2e4c9fb5f78076e6d5e82ad4ebc5d0e10
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-02-13 21:41:26 +0100

    add

--- Etap 5 Typescript/zadanie_koncowe_etapu5/routers/warrior-router.ts
+++ Etap 5 Typescript/zadanie_koncowe_etapu5/routers/warrior-router.ts
@@ -5,9 +5,12 @@ import {Router} from "express";
 export const warriorRouter = Router();
 
 warriorRouter
+
     .get('/register', (req, res):void => {
         res.render('register.hbs',{})
     })
+
     .post('/', (req, res):void => {
-        res.render('hall-of-fame.hbs',{})
+        console.log(req.body)
+        res.render('added.hbs',{})
     })
