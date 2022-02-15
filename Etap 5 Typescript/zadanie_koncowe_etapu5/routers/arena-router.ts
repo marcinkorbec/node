f3163a5522e5bb688e7b990275b5ac7be2c18da0
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-02-15 16:37:20 +0100

    Update arena-router.ts

--- Etap 5 Typescript/zadanie_koncowe_etapu5/routers/arena-router.ts
+++ Etap 5 Typescript/zadanie_koncowe_etapu5/routers/arena-router.ts
@@ -2,6 +2,7 @@
 // formularz wyboru wojownika
 import {Router} from "express";
 import {WarriorRecord} from "../records/warrior.record";
+import {ValidationError} from "../utils/errors";
 
 export const arenaRouter = Router();
 
@@ -13,6 +14,18 @@ arenaRouter
         })
     })
 
-    .post('/fight', (req, res):void => {
+    .post('/fight', async (req, res):Promise<void> => {
+        const {warrior1 :warrior1Id, warrior2: warrior2Id} = req.body;
+
+        const warrior1 = await WarriorRecord.getOne(warrior1Id);
+        const warrior2 = await WarriorRecord.getOne(warrior2Id);
+
+        if (warrior1Id === warrior2Id) {
+            throw new ValidationError("Obydwa wojowniki są takie same, proszę wybrać różne!")
+        }
+
+        if (!warrior1 || !warrior2) {
+            throw new ValidationError("Nie ma takiego użytkownika!")
+        }
         res.render('fight.hbs',{})
     })
