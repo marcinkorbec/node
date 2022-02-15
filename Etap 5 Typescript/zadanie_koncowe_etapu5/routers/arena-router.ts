feabe364a6f5f1f15fbf9c50493bdb709ac23e6f
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-02-15 16:10:32 +0100

    add

--- Etap 5 Typescript/zadanie_koncowe_etapu5/routers/arena-router.ts
+++ Etap 5 Typescript/zadanie_koncowe_etapu5/routers/arena-router.ts
@@ -1,12 +1,16 @@
 // miejsce gdzie będzie pokazana walka
 // formularz wyboru wojownika
 import {Router} from "express";
+import {WarriorRecord} from "../records/warrior.record";
 
 export const arenaRouter = Router();
 
 arenaRouter
-    .get('/fight-form', (req, res):void => {
-        res.render('arena.hbs',{})
+    .get('/fight-form', async (req, res): Promise<void> => {
+        const warriors = await WarriorRecord.listAll();
+        res.render('arena.hbs',{
+            warriors,
+        })
     })
 
     .post('/fight', (req, res):void => {
