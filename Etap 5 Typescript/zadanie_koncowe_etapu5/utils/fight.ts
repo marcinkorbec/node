1f852fac040967d2821f6abc3d5dad8c2df84992
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-02-15 17:25:34 +0100

    Create fight.ts

---
+++ Etap 5 Typescript/zadanie_koncowe_etapu5/utils/fight.ts
@@ -0,0 +1,13 @@
+import {WarriorRecord} from "../records/warrior.record";
+
+export const fight = (warrior1: WarriorRecord, warrior2: WarriorRecord): string[] => {
+    const log:string[] = [];
+
+    let attacker = warrior1;
+    let defender = warrior2;
+
+    do {
+
+    } while ()
+    return log
+}
