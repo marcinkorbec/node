7de1db51fddc1407e3b8c2ff7f563186d2e7851a
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-02-15 12:50:27 +0100

    add

--- Etap 5 Typescript/zadanie_koncowe_etapu5/utils/handlebars-helpers.ts
+++ Etap 5 Typescript/zadanie_koncowe_etapu5/utils/handlebars-helpers.ts
@@ -1,14 +1,5 @@
-helpers:{
-    // Function to do basic mathematical operation in handlebar
-    math: function(lvalue: number, operator:symbol, rvalue:number) {lvalue = parseFloat(lvalue);
-        rvalue = parseFloat(rvalue);
-        return {
-            "+": lvalue + rvalue,
-            "-": lvalue - rvalue,
-            "*": lvalue * rvalue,
-            "/": lvalue / rvalue,
-            "%": lvalue % rvalue
-        }[operator];
-    }
-}}));
-app.set('view engine', 'handlebars');
+// const handlebarsHelpers = {
+//     Handlebars: registerHelper("inc", function(value, options): number {
+//         return parseInt(value) + 1;
+//     });
+// }
