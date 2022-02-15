f93b12474d5c4646451e07612ffe7278ab0fdc25
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-02-15 18:26:45 +0100

    Update fight.ts

--- Etap 5 Typescript/zadanie_koncowe_etapu5/utils/fight.ts
+++ Etap 5 Typescript/zadanie_koncowe_etapu5/utils/fight.ts
@@ -1,13 +1,59 @@
 import {WarriorRecord} from "../records/warrior.record";
 
-export const fight = (warrior1: WarriorRecord, warrior2: WarriorRecord): string[] => {
-    const log:string[] = [];
+export const fight = (warrior1: WarriorRecord, warrior2: WarriorRecord): {
+    log: string[];
+    winner: WarriorRecord;
+} => {
+    const log: string[] = [];
 
-    let attacker = warrior1;
-    let defender = warrior2;
+    const warrior1Obj = {
+        hp: warrior1.resilience * 10,
+        dp: warrior1.defense,
+        warrior: warrior1,
+    };
+
+    const warrior2Obj = {
+        hp: warrior2.resilience * 10,
+        dp: warrior2.defense,
+        warrior: warrior2,
+    };
+
+    let attacker = warrior1Obj;
+    let defender = warrior2Obj;
 
     do {
+        const attackStrength = attacker.warrior.strong;
 
-    } while ()
-    return log
-}
+        log.push(`${attacker.warrior.name} zaatakuje ${defender.warrior.name} z siłą ${attackStrength}.`);
+
+        // DP: 5, HP: 5, attackStrength = 7
+
+        if (defender.dp + defender.warrior.agility > attackStrength) {
+            log.push(`${defender.warrior.name} broni się przed ${attacker.warrior.name}.`);
+
+            defender.dp -= attackStrength;
+
+            // DP: -2, HP: 5
+
+            if (defender.dp < 0) {
+                log.push(`${attacker.warrior.name} przełamał obronę ${defender.warrior.name} zadając mu ${-defender.dp} obrażeń.`);
+                defender.hp += defender.dp;
+
+                // HP = 5 - 2 = 3
+            }
+        } else {
+            log.push(`${attacker.warrior.name} zadał ${attackStrength} obrażeń ${defender.warrior.name}.`);
+            defender.hp -= attackStrength;
+        }
+
+        [defender, attacker] = [attacker, defender];
+    } while (defender.hp > 0);
+
+    const winner = defender.warrior;
+    log.push(`${winner.name} zwyciężył!`);
+
+    return {
+        log,
+        winner,
+    };
+};
