de005c7d1c20901f41f44efd0a4197f6e09ba6f5
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-03-03 20:20:25 +0100

    add

--- React - twórca stron/Nowy folder/app.js
+++ React - twórca stron/Nowy folder/app.js
@@ -1,4 +1,4 @@
 ReactDOM.render(
-    (<div><h1>Hello Roman!</h1> <p>Tekst pod nagłówkiem.</p></div>),
+    <div><h1>Hello Roman!</h1> <p>Tekst pod nagłówkiem.</p></div>,
     document.getElementById('root')
 );
