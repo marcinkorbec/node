f75c3a1291100274aab14921755a45ab7367b023
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-03-04 22:39:02 +0100

    add

--- React - twórca stron/Nowy folder/app.js
+++ React - twórca stron/Nowy folder/app.js
@@ -1,14 +1,26 @@
-const Title = () => <h1>Mój tytuł</h1>;
-const MyComponent = function() {
+const Title = () => <h1>To jest blog o gotowaniu</h1>;
+
+const Post = () => {
+
+}
+
+const Blog = function() {
     return (
         <div>
-            <Title></Title>
-            <p>Tekst pod nagłówkiem</p>
+            <Title/>
+            <h2>Przepis na naleśniki.</h2>
+            <p>Na naturalnym mleku...</p>
+            <button>Czytaj więcej</button>
+            <hr/>
+            <h2>Przepis na Kurczaka.</h2>
+            <p>Na ostro...</p>
+            <button>Czytaj więcej</button>
+            <hr/>
         </div>
     )
 }
 
 ReactDOM.render(
-    <MyComponent/>,
+    <Blog/>,
     document.getElementById('root')
 );
