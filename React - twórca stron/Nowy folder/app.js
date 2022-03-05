542cc65571258fe100e1a562dcfdd85651d13484
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-03-05 20:27:10 +0100

    add

--- React - twórca stron/Nowy folder/app.js
+++ React - twórca stron/Nowy folder/app.js
@@ -5,6 +5,7 @@ const Post = (props) => {
         <>
             <h2>{props.title}</h2>
             <p>{props.shortContent}</p>
+            <p>{props.content}</p>
             <button>Czytaj więcej</button>
         <hr/>
         </> 
@@ -12,28 +13,37 @@ const Post = (props) => {
 }
 
 const Blog = function() {
-const posts = [
-    {
-        title: "Przepis na naleśniki.",
-        shortContent: "Na naturalnym mleku..."
-    },
-    {
-        title: "Przepis na Kurczaka.",
-        shortContent: "Na ostro..."
-    }
-];
+    const posts = [
+        {
+            title: "Przepis na naleśniki.",
+            shortContent: "Na naturalnym mleku...",
+            content: 'mleko, jaja zmieszać, dodać cukru i będzie zajebiście...',
+            showContent: true,
+            id: 2
+        },
+        {
+            title: "Przepis na Kurczaka.",
+            shortContent: "Na ostro...",
+            content: 'pokroić kurczaka, usmażyć i tak dalej...',
+            showContent: false,
+            id: 3
+        }
+    ];
+
+    const postsList = posts.map(el => (
+        <Post
+            key= {el.id}
+            title={el.title}
+            content={el.content}
+            shortContent={el.shortContent}
+            shortContent={el.shortContent}
+        />
+    ))
 
     return (
         <div>
             <Title/>
-            <Post 
-                title={posts.title}
-                shortContent={posts.shortContent}>
-            </Post>
-            <Post 
-                title= {posts.title}
-                shortContent= {posts.shortContent}>
-            </Post>
+            {postsList}
         </div>
     )
 }
