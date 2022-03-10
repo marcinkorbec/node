d11371d58d2efa592ce265feaff3a88b26fadf64
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-03-10 18:32:36 +0100

    add changes

--- Etap 6 React/my-first-react-app/my-first-app/src/index.js
+++ Etap 6 React/my-first-react-app/my-first-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 import './index.css';
-import Participants from './Participants';
+import {Participants} from './Participants';
 
 const data = [
 	{
