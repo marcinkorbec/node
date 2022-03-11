9bc855a2971cb765b149ecc5a60df9b447f7a2db
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-03-11 19:17:56 +0100

    add

--- Etap 6 React/my-first-react-app/my-first-app/src/RecordsCount.js
+++ Etap 6 React/my-first-react-app/my-first-app/src/RecordsCount.js
@@ -1,18 +1,8 @@
 import React from "react";
 import './App.css';
-import {StringsList} from "./StringsList";
 
 
-
-export const Participants = (props) => {
-	const names = props.list.map(person => person.name)
-	console.log(names);
-
-	return (
-		<>
-			<StringsList list={names}/>
-			<hr/>
-			<p>Baza zawiera {props.list.length} rekordów.</p>
-		</>
-	);
+export const RecordsCount = (props) => {
+	console.log(props)
+	return <p>Razem: {props.list.length} rekordów.</p>
 }
