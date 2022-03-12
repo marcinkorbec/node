a85edc369c7dd49fbe0d15735337ca368e1a878d
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-03-12 18:11:17 +0100

    update

--- Etap 6 React/my-first-react-app/my-first-app/src/StringsList.js
+++ Etap 6 React/my-first-react-app/my-first-app/src/StringsList.js
@@ -4,8 +4,6 @@ import './App.css';
 
 export const StringsList = (props) => {
 
-	const [dontGiveUp, setDontGiveUp] = useState(true);
-
 	const string = props.list
 		.slice(0, props.list.length - 1)
 		.join(', ');
