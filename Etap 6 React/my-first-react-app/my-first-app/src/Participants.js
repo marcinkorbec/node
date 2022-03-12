f3f661475229dc3a3fe2cc9a5b2d4942c7c77bec
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-03-12 23:09:21 +0100

    update

--- Etap 6 React/my-first-react-app/my-first-app/src/Participants.js
+++ Etap 6 React/my-first-react-app/my-first-app/src/Participants.js
@@ -6,13 +6,16 @@ import './App.css';
 export const Participants = (props) => {
 
 	const [thingToDo, setThingToDo] = useState('Iść do lodówki oraz');
+	let secondInfo = 'Kreatywnośc nie zna granic. '
 
 	setTimeout(() => {
-		setThingToDo('zjeść kiełbase')
+		setThingToDo('zjeść kiełbase');
+		secondInfo = 'xyz';
 	}, 5000)
 
 	return (
 		<>
+			<div>{secondInfo}</div>
 			{thingToDo}
 		</>
 	);
