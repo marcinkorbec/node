b669223ddc78ee81c1b4985b116c01799db47c91
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-03-12 15:48:25 +0100

    update

---
+++ Etap 6 React/my-first-react-app/my-first-app/src/data/participants.js
@@ -0,0 +1,18 @@
+export const data = [
+	{
+		name: 'Anna',
+		mail: 'a@wp.pl'
+	},
+	{
+		name: 'Maria',
+		mail: 'm@wp.pl'
+	},
+	{
+		name: 'Ela',
+		mail: 'e@wp.pl'
+	},
+	{
+		name: 'Józia',
+		mail: 'j@wp.pl'
+	}
+];
