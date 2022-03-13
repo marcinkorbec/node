32d2ad801df6812351b1fff148466ace38217b68
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-03-13 21:55:10 +0100

    update lesson Etap 6 Tydzien 2 Dzien 4

--- Etap 6 React/my-first-react-app/my-first-app/src/Participants.js
+++ Etap 6 React/my-first-react-app/my-first-app/src/Participants.js
@@ -9,13 +9,27 @@ export class Participants extends Component {
 		lastName: 'Xxxx'
 	};
 
-	render () {
-
+	componentDidMount() {
 		setTimeout( () => {
 			this.setState({
 				name: 'Bar'
 			})
 		}, 2500)
+
+		console.log('Pierwsze zamontowanie naszego komponentu.');
+	}
+
+	componentDidUpdate() {
+		console.log('Aktualizowanie DOMu.')
+	}
+
+	componentWillUnmount() {
+		console.log('Komponent zostanie odmonotowany.')
+	}
+
+	render () {
+		const name = this.state;
+
 
 		return <h1>{this.state.name}</h1>
 	}
