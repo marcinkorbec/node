a3c4236f63c14ad4d22f4bf541890b09278949c4
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-03-14 10:52:52 +0100

    update lesson Etap 6 Tydzien 2 Dzien 4

--- Etap 6 React/my-first-react-app/my-first-app/src/index.js
+++ Etap 6 React/my-first-react-app/my-first-app/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {render} from 'react-dom';
 import './index.css';
-import {Participants} from './Participants';
+import {Counter} from './Counter';
 import {data} from "./data/participants"
 
 render(
 	<React.StrictMode>
-		<Participants list={data}/>
+		<Counter list={data}/>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
