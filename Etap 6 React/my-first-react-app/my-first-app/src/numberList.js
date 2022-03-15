7a7a46cca19c1e2fdf98b6b0164f7ac41c4785e3
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-03-15 18:29:15 +0100

    update lesson Etap 6 Tydzien 2 Dzien 5 zad 1

--- Etap 6 React/my-first-react-app/my-first-app/src/numberList.js
+++ Etap 6 React/my-first-react-app/my-first-app/src/numberList.js
@@ -9,21 +9,23 @@ export class NumberList extends Component {
 		numbers: [1],
 	};
 
-	// componentDidMount() {
-	// 	this.intervalId = setInterval( () => {
-	// 		this.setState(prev => ({
-	// 			count: prev.count + 1,
-	// 		}))
-	// 	}, 1000)
-	// 	console.log('Pierwsze zamontowanie naszego komponentu.');
-	// }
-	//
-	// componentWillUnmount() {
-	// 	console.log('Komponent zostanie odmonotowany.')
-	// 	clearInterval(this.intervalId);
-	// }
+	componentDidMount() {
+		this.intervalId = setInterval( () => {
+			this.setState(prev => ({
+				numbers: [...prev.numbers, prev.numbers.length + 1]
+			}));
+		}, 1000)
+
+		console.log('Pierwsze zamontowanie naszego komponentu.');
+	}
+
+	componentWillUnmount() {
+		console.log('Komponent zostanie odmonotowany.')
+		clearInterval(this.intervalId);
+	}
 
 	render () {
 		return <StringsList list={this.state.numbers}/>
 	}
 }
+
