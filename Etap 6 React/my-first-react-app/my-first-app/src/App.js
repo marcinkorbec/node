91a6dafbcefe2c6a4d45026a24ac1bb218d45535
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-03-15 16:40:33 +0100

    update lesson Etap 6 Tydzien 2 Dzien 5

--- Etap 6 React/my-first-react-app/my-first-app/src/App.js
+++ Etap 6 React/my-first-react-app/my-first-app/src/App.js
@@ -3,12 +3,5 @@ import './App.css';
 import {NumberList} from "./numberList";
 
 export const App = props => {
-	const [show, setShow] = useState(true);
-
-	setTimeout(()=> {
-		setShow(false);
-	}, 5000)
-
-
-	return show===true ? <NumberList/> : null;
+	<NumberList/>
 }
