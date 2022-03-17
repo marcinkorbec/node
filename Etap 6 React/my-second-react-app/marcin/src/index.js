8fe427e596314dd16e6bfa1bc92d2d83f0cb15b4
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-03-17 20:24:48 +0100

    update

--- Etap 6 React/my-second-react-app/marcin/src/index.js
+++ Etap 6 React/my-second-react-app/marcin/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {App} from './App';
 import reportWebVitals from './reportWebVitals';
-import {Clock} from "./Clock";
-import {NumberList} from "./NumbersList";
 
 ReactDOM.render(
   <React.StrictMode>
