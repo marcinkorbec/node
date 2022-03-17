1480359cf3fff051501ea784b7c55d4384e60b0a
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-03-17 16:20:31 +0100

    update

---
+++ Etap 6 React/my-second-react-app/marcin/src/NumbersList.js
@@ -0,0 +1,31 @@
+import React, {Component} from "react";
+import './App.css';
+import {StringsList} from "./StringsList";
+
+
+
+export class NumberList extends Component {
+    state = {
+        numbers: [1],
+    };
+
+    componentDidMount() {
+        this.intervalId = setInterval( () => {
+            this.setState(prev => ({
+                numbers: [...prev.numbers, prev.numbers.length + 1]
+            }));
+        }, 10000)
+
+        console.log('Pierwsze zamontowanie naszego komponentu.');
+    }
+
+    componentWillUnmount() {
+        console.log('Komponent zostanie odmonotowany.')
+        clearInterval(this.intervalId);
+    }
+
+    render () {
+        return <StringsList list={this.state.numbers}/>
+    }
+}
+
