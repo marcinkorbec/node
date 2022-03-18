f042c30b371f83db2114a5ae5ba017ed6cf703f8
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-03-18 22:15:26 +0100

    update

--- Etap 6 React/my-second-react-app/marcin/src/Progressbar.js
+++ Etap 6 React/my-second-react-app/marcin/src/Progressbar.js
@@ -1,3 +1,32 @@
-export const Progressbar = props => {
+import React, {useEffect, useState} from "react";
 
+import './Progressbar.css'
+
+export const Progressbar = props => {
+    const [progressbar, setProgressbar] = useState(0);
+
+    useEffect(()=> {
+        const intervalId = setInterval(()=> {
+            setProgressbar( progressbar => {
+                if (progressbar >= 99) {
+                    clearInterval(intervalId)
+                    return progressbar;
+                }
+                return progressbar + 0.05;
+                }
+        )}, 1);
+
+        return ()=> clearInterval(intervalId);
+    },[])
+
+    return (
+        <div
+        className="Progressbar"
+        style={{
+            width: `${progressbar}vw`
+        }}
+    >{Math.ceil(progressbar)}%</div>
+    )
 }
+
+// progressbar>=100? progressbar = 100 : Math.ceil(progressbar)
