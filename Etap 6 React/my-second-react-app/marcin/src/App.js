7e8e06c5fbed768971f1a69ef481b50fbfdda165
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-03-18 20:29:02 +0100

    update

--- Etap 6 React/my-second-react-app/marcin/src/App.js
+++ Etap 6 React/my-second-react-app/marcin/src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 import {CrazyInput} from "./CrazyInput";
 
 export const App = props => (
+
     <>
+        <div>Marcin Korbecki</div>
         <CrazyInput />
-        <CrazyInput isPwd/>
     </>
 )
