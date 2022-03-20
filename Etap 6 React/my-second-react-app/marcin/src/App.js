ef4be0b97a83af9f8ab059f4f2706b425faca9bf
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-03-20 11:10:05 +0100

    update

--- Etap 6 React/my-second-react-app/marcin/src/App.js
+++ Etap 6 React/my-second-react-app/marcin/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import {ThreeColorsLedBox} from "./Zad2";
+import {PassText} from "./Zad1";
 
 
 export const App = props => (
 
     <>
-        <ThreeColorsLedBox/>
+        <PassText/>
     </>
 )
