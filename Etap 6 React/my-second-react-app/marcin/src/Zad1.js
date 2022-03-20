6b920c2060fcfe8f879e763ec641eebd0a03c4ed
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-03-20 11:10:37 +0100

    update

---
+++ Etap 6 React/my-second-react-app/marcin/src/Zad1.js
@@ -0,0 +1,10 @@
+import React, {useState} from "react";
+import "./Zad1.css";
+
+export const PassText = props => {
+    const [] = useState(onMouseUp)
+    
+    return (
+        <input type="text"/>
+    )
+}
