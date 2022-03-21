00b0ff30c195def18ddb1a83ed383ea6c580e11c
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-03-21 21:06:28 +0100

    update project

---
+++ Etap 6 React/my-3-react-app/project-combo/src/components/UsersList/UsersList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {UsersListOneItem} from "./UsersListOneItem";
+
+export const UsersList = props => {
+
+    return (
+        <div className="container">
+            <div className="user-list">
+                {
+                    [...props.users]
+                    .sort((a, b)=> a.username.localeCompare(b.username))
+                    .map(userObj => <UsersListOneItem user={userObj} key={userObj.username}/>)
+                }
+            </div>
+        </div>
+    )
+}
