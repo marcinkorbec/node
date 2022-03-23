0fb833e5dd5b1fed49e6ff8e8e56079337528083
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-03-23 16:29:19 +0100

    update project

--- Etap 6 React/my-3-react-app/project-combo/src/components/App/App.js
+++ Etap 6 React/my-3-react-app/project-combo/src/components/App/App.js
@@ -1,11 +1,25 @@
 import {UsersList} from "../UsersList/UsersList";
 import {People} from "../../data/people";
 import './App.css';
+import {useState} from "react";
 
 export const App = props => {
+  const [firstName, setFirstName] = useState('')
+
+  const changeFirstName = e => {
+    setFirstName(e.target.value);
+  }
+
+  const backgroundColor = firstName[0] === '@' ? 'lime' : '#fab7b7';
+
   return (
-      <UsersList users={People}/>
+    <>
+      <input type="password" value={firstName} onChange={changeFirstName} style={{backgroundColor: `${backgroundColor}`}}/>
+      <h1>{firstName}</h1>
+      <p>Wpisałeś {firstName.length} znaków.</p>
+    </>
   );
 }
 
+// <UsersList users={People}/>
 
