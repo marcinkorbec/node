9e4b86f4ede51bf90a11fa195f5300c5473bf36e
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-03-23 16:29:55 +0100

    update project

--- Etap 6 React/my-3-react-app/project-combo/src/components/UsersList/UsersListOneItem.js
+++ Etap 6 React/my-3-react-app/project-combo/src/components/UsersList/UsersListOneItem.js
@@ -2,12 +2,18 @@ import React from "react";
 import {capitalizeFirstLetter} from "./CapitalizeFirstLetter";
 import "./UsersList.css"
 
-export const UsersListOneItem = props => (
-    <div className="user">
+export const UsersListOneItem = props => {
+
+    const oneUserClicked = ()=> {
+        props.onRemoveItem(props.user.username);
+    }
+
+    return (
+    <div className="user" onClick={oneUserClicked}>
         <img className="user_image" src={`https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/${props.user.picture}`}/>
         <div className="user_name">
             <strong>{capitalizeFirstLetter(props.user.first_name)} {capitalizeFirstLetter(props.user.last_name)} </strong>
             <br/> <em>{props.user.username}</em>
         </div>
     </div>
-)
+)}
