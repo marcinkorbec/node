472d2ae1a0364aa5f32abe612385416ca5539203
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-03-26 14:42:04 +0100

    update project

---
+++ Etap 6 React/my-3-react-app/project-combo/src/components/Text/Text.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Dialog} from "../common/Dialog/Dialog";
+
+export const TextApp = props => {
+  return (
+    <>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad est facilis inventore ipsum labore minus, molestiae mollitia nam nisi quasi quibusdam recusandae, repellendus voluptas. Consequatur consequuntur qui sint velit veritatis?</p>
+      <Dialog body="Witam serdetsznie na stronie głównej :)"/>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi asperiores assumenda aut commodi culpa doloremque dolores enim, error id laboriosam magnam natus nemo nihil nulla pariatur, quam ullam, ut voluptatibus.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio inventore nesciunt odio porro saepe vitae. A, dignissimos doloremque eaque facere hic maiores modi obcaecati odio quasi, quia quos, sit veniam.</p>
+    </>
+  )
+}
