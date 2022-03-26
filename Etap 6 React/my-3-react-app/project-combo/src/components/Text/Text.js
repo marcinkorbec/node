756ab17e409cc847e1b38f97f7a06ed3f0851dbf
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-03-26 21:41:13 +0100

    update project

--- Etap 6 React/my-3-react-app/project-combo/src/components/Text/Text.js
+++ Etap 6 React/my-3-react-app/project-combo/src/components/Text/Text.js
@@ -1,19 +1,18 @@
 import React from "react";
 import {Dialog} from "../common/Dialog/Dialog";
-import {Dialog2} from "../Dialog2/Dialog2";
+import {ConfirmDialog} from "../common/Dialog/ConfirmDialog";
 
 export const TextApp = props => {
   return (
     <>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad est facilis inventore ipsum labore minus, molestiae mollitia nam nisi quasi quibusdam recusandae, repellendus voluptas. Consequatur consequuntur qui sint velit veritatis?</p>
-      <Dialog>Witam serdetsznie na stronie głównej :)</Dialog>
-      <Dialog>
+      <Dialog title="Witam">Witam serdetsznie na stronie głównej sialalala:)</Dialog>
+      <Dialog title="Jest pięknie a będzie jeszcze piękniej">
         <h1>Tytuł</h1>
-        <p>sialala</p>
+        <p>sialala, jest legancko</p>
       </Dialog>
-      <Dialog2 body='Jedziemy po zielsko!!!'/>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi asperiores assumenda aut commodi culpa doloremque dolores enim, error id laboriosam magnam natus nemo nihil nulla pariatur, quam ullam, ut voluptatibus.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio inventore nesciunt odio porro saepe vitae. A, dignissimos doloremque eaque facere hic maiores modi obcaecati odio quasi, quia quos, sit veniam.</p>
+      <ConfirmDialog title="Czy aby napewno?">
+        <p>Kilnij aby zatwierdzić:</p>
+      </ConfirmDialog>
     </>
   )
 }
