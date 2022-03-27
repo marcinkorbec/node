fec1e197b6f2e40a0eb1df193d35b1fade252dc9
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-03-27 23:59:09 +0200

    update react app

--- Etap 6 React/my-3-react-app/project-combo/src/components/UsersList/UsersList.js
+++ Etap 6 React/my-3-react-app/project-combo/src/components/UsersList/UsersList.js
@@ -2,29 +2,29 @@ import React, {useState} from "react";
 import {UsersListOneItem} from "./UsersListOneItem";
 
 export const UsersList = props => {
-    const [list, setList] = useState(props.users);
+  const [list, setList] = useState(props.users);
 
-    const removeItem = (username) => {
-        console.log("Usuń element.", username)
-        setList(list => (list
-            .filter(one => one.username !== username)
-            // [1,2,3].filter(one => one !== 2) // [1,3]
-        ))
-    }
+  const removeItem = (username) => {
+    console.log("Usuń element.", username)
+    setList(list => (list
+        .filter(one => one.username !== username)
+      // [1,2,3].filter(one => one !== 2) // [1,3]
+    ))
+  }
 
-    return (
-        <div className="container">
-            <div className="user-list">
-                {
-                    [...list]
-                    .sort((a, b)=> a.username.localeCompare(b.username))
-                    .map(userObj => <UsersListOneItem
-                        user={userObj}
-                        key={userObj.username}
-                        onRemoveItem={removeItem}
-                    />)
-                }
-            </div>
-        </div>
-    )
+  return (
+    <div className="container">
+      <div className="user-list">
+        {
+          [...list]
+            .sort((a, b) => a.username.localeCompare(b.username))
+            .map(userObj => <UsersListOneItem
+              user={userObj}
+              key={userObj.username}
+              onRemoveItem={removeItem}
+            />)
+        }
+      </div>
+    </div>
+  )
 }
