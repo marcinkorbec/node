92165de622eb05fe7afe867a577dd81637810685
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-03-27 23:59:19 +0200

    update react app

--- Etap 6 React/my-3-react-app/project-combo/src/components/UsersList/UsersListOneItem.js
+++ Etap 6 React/my-3-react-app/project-combo/src/components/UsersList/UsersListOneItem.js
@@ -4,16 +4,18 @@ import "./UsersList.css"
 
 export const UsersListOneItem = props => {
 
-    const oneUserClicked = ()=> {
-        props.onRemoveItem(props.user.username);
-    }
+  const oneUserClicked = () => {
+    props.onRemoveItem(props.user.username);
+  }
 
-    return (
+  return (
     <div className="user" onClick={oneUserClicked}>
-        <img className="user_image" src={`https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/${props.user.picture}`}/>
-        <div className="user_name">
-            <strong>{capitalizeFirstLetter(props.user.first_name)} {capitalizeFirstLetter(props.user.last_name)} </strong>
-            <br/> <em>{props.user.username}</em>
-        </div>
+      <img className="user_image"
+           src={`https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/${props.user.picture}`}/>
+      <div className="user_name">
+        <strong>{capitalizeFirstLetter(props.user.first_name)} {capitalizeFirstLetter(props.user.last_name)} </strong>
+        <br/> <em>{props.user.username}</em>
+      </div>
     </div>
-)}
+  )
+}
