4fa69ad9d4d4b5ce8b3f4309f99f686ad5bc524c
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-03-29 14:05:21 +0200

    update react typescript project

--- Etap 6 React/my-4-react-app/marcin/src/App.tsx
+++ Etap 6 React/my-4-react-app/marcin/src/App.tsx
@@ -2,27 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Dialog} from "./components/common/Dialog/Dialog";
+import {CryptoPrice} from "./components/CryptoPrice/CryptoPrice";
 
 export const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-          <Dialog>
-              <h1>Wstawić można tu coś!</h1>
-          </Dialog>
-      </header>
+        <Dialog>
+            <h1>Wstawić można tu coś!</h1>
+        </Dialog>
+        <CryptoPrice/>
     </div>
   );
 }
