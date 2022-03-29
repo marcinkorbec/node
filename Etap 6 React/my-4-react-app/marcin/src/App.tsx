8c94340c27472a79c650ab514171e95c51a6af73
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-03-29 15:37:13 +0200

    update react typescript project

--- Etap 6 React/my-4-react-app/marcin/src/App.tsx
+++ Etap 6 React/my-4-react-app/marcin/src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Dialog} from "./components/common/Dialog/Dialog";
 import {CryptoPrice} from "./components/CryptoPrice/CryptoPrice";
 
 export const App = () => {
   return (
     <div className="App">
-        <Dialog>
-            <h1>Wstawić można tu coś!</h1>
-        </Dialog>
         <CryptoPrice/>
     </div>
   );
