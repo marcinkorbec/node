00c755c9e6e6db662d61fe523244660ae4f40d83
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-03-30 20:00:26 +0200

    Delete home.ts

--- Etap 6 React/nasze_własne_API_i_konfiguracja/bakend lista mikołaja/routers/home.ts
+++
@@ -1,9 +0,0 @@
-import {Request, Response, Router} from "express";
-
-export const homeRouter = Router();
-
-homeRouter
-	.get('/', (req: Request, res: Response) => {
-		res.redirect('/child');
-	});
-
