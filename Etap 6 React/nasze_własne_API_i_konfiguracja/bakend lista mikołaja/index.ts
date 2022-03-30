c3e9661873d556a97b8c2f4bf76e8e4c1326959b
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-03-30 19:49:59 +0200

    Update index.ts

--- Etap 6 React/nasze_własne_API_i_konfiguracja/bakend lista mikołaja/index.ts
+++ Etap 6 React/nasze_własne_API_i_konfiguracja/bakend lista mikołaja/index.ts
@@ -1,31 +1,21 @@
 import * as express from"express";
+import * as cors from 'cors';
 import 'express-async-errors';
-import * as methodOverride  from "method-override";
-import {engine} from "express-handlebars";
 import {handleError} from "./utils/errors";
-import {homeRouter} from "./routers/home";
 import {childRouter} from "./routers/child";
 import {giftRouter} from "./routers/gift";
-import {handlebarsHelpers} from "./utils/handlebars-helpers";
 import'./utils/db';
 
 const app = express();
 
-app.use(methodOverride('_method'));
-// app.use(express.urlencoded({
-//     extended: true,
-// }));
-// app.use(express.static('public'));
 app.use(express.json()); //Content-type: application/json
-// app.engine('.hbs', engine({
-//     extname: '.hbs',
-//     helpers: handlebarsHelpers, // Dodatkowe funkcjonalności, które chcemy dodać do Handlebarsów
-// }));
-// app.set('view engine', '.hbs');
 
-app.use('/', homeRouter);
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
+
 app.use('/child', childRouter);
-app.use('/gift', giftRouter);
+app.use('/', giftRouter);
 
 app.use(handleError);
 
