8b15cd9d1d6f65c7277dc5dd8fc950b65fb3e44c
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-04-02 00:36:46 +0200

    Update gift.ts

--- Etap 6 React/nasze_własne_API_i_konfiguracja/bakend lista mikołaja/routers/gift.ts
+++ Etap 6 React/nasze_własne_API_i_konfiguracja/bakend lista mikołaja/routers/gift.ts
@@ -1,5 +1,6 @@
 import {Router, Request, Response} from "express";
 import {GiftRecord} from "../records/gift.record";
+import {ValidationError} from "../utils/errors";
 
 export const giftRouter = Router();
 
@@ -14,10 +15,18 @@ giftRouter
 	})
   
   .delete('/:id', async (req, res) => {
-  
-  }
-  
-  )
+    const gift = await GiftRecord.getOne(req.params.id);
+    
+    if (!gift) {
+      throw new ValidationError('Próbujesz usunąć coś czego nie ma!')
+    }
+    
+    if (await gift.countGivenGifts() > 0) {
+      throw new ValidationError('Nie można usunąć tego co jest używane!')
+    }
+    
+    res.end();
+  })
 
 	.post('/', async (req: Request, res: Response): Promise<void> => {
 		const data = {
