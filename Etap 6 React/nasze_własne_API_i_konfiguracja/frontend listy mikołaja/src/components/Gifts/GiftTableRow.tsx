d63c104ae0a28b999a30b9ca404ce3e5da1c2efe
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-04-02 19:45:25 +0200

    Update GiftTableRow.tsx

--- Etap 6 React/nasze_własne_API_i_konfiguracja/frontend listy mikołaja/src/components/Gifts/GiftTableRow.tsx
+++ Etap 6 React/nasze_własne_API_i_konfiguracja/frontend listy mikołaja/src/components/Gifts/GiftTableRow.tsx
@@ -1,20 +1,44 @@
-import React, {useEffect, useState} from "react";
+import React, {MouseEvent, useEffect, useState} from "react";
 import {GiftEntity} from "types";
 
 
 interface Props {
   gift: GiftEntity;
   index: number;
+  onGiftsChange: () => void;
 }
 
 
-export const GiftTableRow = (props: Props) => (
-  <tr>
-    <td>{props.index + 1}</td>
-    <td>{props.gift.name}</td>
-    <td>{props.gift.count} szt.</td>
-    <td><a href="#">🗑</a></td>
-  </tr>
-)
+
+export const GiftTableRow = (props: Props) => {
+  const deleteGift = async (event: MouseEvent) => {
+    event.preventDefault();
+    
+    if(!window.confirm(`Czy jesteś pewny że chcesz usunąć ${props.gift.name}?`)) {
+      return;
+    }
+    
+    const res = await fetch(`http://localhost:3002/${props.gift.id}`, {
+      method: 'DELETE',
+    })
+    
+    if (res.status === 400 || res.status === 500) {
+      const error = await res.json();
+      alert(`Error occured: ${error.message}`)
+    }
+    
+    props.onGiftsChange();
+    console.log(res);
+  }
+  
+  return (
+    <tr>
+      <td>{props.index + 1}</td>
+      <td>{props.gift.name}</td>
+      <td>{props.gift.count} szt.</td>
+      <td><a href="#" onClick={deleteGift}>🗑</a></td>
+    </tr>
+  )
+}
 
 
