11f43df3359674b55efd2f1b5b85973336db0d55
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-04-11 22:11:06 +0200

    Update child.ts

--- Etap 6 React/nasze_własne_API_i_konfiguracja/bakend lista mikołaja/routers/child.ts
+++ Etap 6 React/nasze_własne_API_i_konfiguracja/bakend lista mikołaja/routers/child.ts
@@ -2,6 +2,7 @@ import {Router, Request, Response} from "express";
 import {ChildRecord} from "../records/child.record";
 import {GiftRecord} from "../records/gift.record";
 import {ValidationError} from "../utils/errors";
+import {CreateChildReq, ListChildrenRes, SetGiftForChildReq} from "../types";
 
 export const childRouter = Router();
 
@@ -14,24 +15,28 @@ childRouter // /child
         res.json({
             childrenList,
             giftsList,
-        });
+        } as ListChildrenRes);
     })
 
     .post('/', async (req: Request, res: Response): Promise<void> => {
-        const newChild = new ChildRecord(req.body);
+        const newChild = new ChildRecord(req.body as CreateChildReq);
         await newChild.insert();
 
-        res.redirect('/child');
+        res.json(newChild);
     })
 
     .patch('/gift/:childId', async (req: Request, res: Response): Promise<void> => {
+        const {body}: {
+          body: SetGiftForChildReq;
+        } = req;
+      
         const child = await ChildRecord.getOne(req.params.childId);
 
         if (child === null) {
             throw new ValidationError('Nie znaleziono dziecka z podanym ID.');
         }
 
-        const gift = req.body.giftId === '' ? null : await GiftRecord.getOne(req.body.giftId);
+        const gift = (req.body as SetGiftForChildReq).giftId === '' ? null : await GiftRecord.getOne(req.body.giftId);
 
         if (gift) {
             if (gift.count <= await gift.countGivenGifts()) {
@@ -42,6 +47,6 @@ childRouter // /child
         child.giftId = gift?.id ?? null;
         await child.update();
 
-        res.redirect('/child');
+        res.json(child);
     })
 
