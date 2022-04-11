06d31479e43e5050f9991be6085cddc5ffeae9e1
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-04-11 22:11:16 +0200

    Update child.ts

--- Etap 6 React/nasze_własne_API_i_konfiguracja/bakend lista mikołaja/types/child/child.ts
+++ Etap 6 React/nasze_własne_API_i_konfiguracja/bakend lista mikołaja/types/child/child.ts
@@ -1,3 +1,13 @@
 import {ChildEntity} from "./child.entity";
+import {GiftEntity} from "../gift";
 
 export type CreateChildReq = Omit<ChildEntity, 'id'>;
+
+export interface ListChildrenRes {
+  childrenList: ChildEntity[];
+  giftsList: GiftEntity[];
+}
+
+export interface SetGiftForChildReq {
+  giftId: string;
+}
