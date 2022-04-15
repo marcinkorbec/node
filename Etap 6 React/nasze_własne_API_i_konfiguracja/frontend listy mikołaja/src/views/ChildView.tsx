fa44decc47cb0d6d3f8f05c01bb57e0da9dd957b
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-04-15 16:00:42 +0200

    stan na E6T5D5

--- Etap 6 React/nasze_własne_API_i_konfiguracja/frontend listy mikołaja/src/views/ChildView.tsx
+++ Etap 6 React/nasze_własne_API_i_konfiguracja/frontend listy mikołaja/src/views/ChildView.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import {Link, useParams} from "react-router-dom";
+import {ChildrenList} from "../components/Children/ChildrenList";
+import {AddChild} from "../components/AddChild/AddChild";
 
 export const ChildView = () => {
   const {foobar} = useParams();
@@ -9,6 +11,8 @@ export const ChildView = () => {
   return (
     <>
       <h1>Dzieci:</h1>
+      <ChildrenList/>
+      <AddChild/>
       <Link to="/child">Przełącz do Prezentów.</Link>
     </>
   )
