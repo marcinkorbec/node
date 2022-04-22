c28276517e661d566978bef97f5d255cd89e7fcd
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-04-22 21:25:26 +0200

    E7T1D2

--- Etap 7 Advanced/1/bakend lista mikołaja/test.ts
+++ Etap 7 Advanced/1/bakend lista mikołaja/test.ts
@@ -1,10 +1,26 @@
 import {strict as assert} from "assert";
+import {buildPerson} from "./utils/foobar";
 
-assert.deepStrictEqual({
-    name: 'Ziutek',
-    surname: 'Bezbutek',
-  },
+assert.doesNotThrow(
+  () => buildPerson('Marcin','Testowy'),
+  'buildPerson() throws. '
+);
+
+assert.deepStrictEqual(
+  buildPerson('Ziutek', 'Ziutkowy'),
   {
     name: 'Ziutek',
     surname: 'Ziutkowy',
   }, 'To nie są te same osoby.');
+
+// import {strict as assert} from "assert";
+//
+// assert.deepStrictEqual([1,2,3], [1,3,2])
+
+
+// assert.rejects(
+//   () => buildPerson('',''),
+//   {
+//     message: 'Name and surname should not be empty.'
+//   },
+//   'buildPerson() does not throw message when empty name or surname. ');
