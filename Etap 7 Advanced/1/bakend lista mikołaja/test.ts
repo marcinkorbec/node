1c20734328663e513b526db4f5b94f3aec47f5b4
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-04-22 13:43:08 +0200

    E7T1D2

--- Etap 7 Advanced/1/bakend lista mikołaja/test.ts
+++ Etap 7 Advanced/1/bakend lista mikołaja/test.ts
@@ -1,3 +1,10 @@
 import {strict as assert} from "assert";
 
-assert.deepStrictEqual({ a: 1 }, { a: '1' });
+assert.deepStrictEqual({
+    name: 'Ziutek',
+    surname: 'Bezbutek',
+  },
+  {
+    name: 'Ziutek',
+    surname: 'Ziutkowy',
+  }, 'To nie są te same osoby.');
