dc99a9611a33a9b0c5c3d80425342dddb454e631
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-04-24 16:47:17 +0200

    E7T1D4

---
+++ Etap 7 Advanced/1/bakend lista mikołaja/tests/test.test.ts
@@ -0,0 +1,12 @@
+test('adds 1 + 2 to equal 3', ()=> {
+  expect(1 + 2).toBe(3)
+})
+
+test('adds 1 + 2 to equal 4', ()=> {
+  expect(1 + 2).toBe(4)
+})
+
+
+test('Sprawdzamy czy buildPerson jest prawdziwawy', () => {
+  expect(2).toBeTruthy();
+});
