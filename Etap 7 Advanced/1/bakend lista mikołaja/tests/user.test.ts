4ee7c99022db4d73ea0ab77deaffce960c6bf6c2
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-04-25 23:04:22 +0200

    E7T1D5

---
+++ Etap 7 Advanced/1/bakend lista mikołaja/tests/user.test.ts
@@ -0,0 +1,27 @@
+import {User} from "../records/user";
+
+let user: User;
+
+beforeAll(async ()=> {
+  user = new User();
+})
+
+test('User should not be logged in at the beginning',  ()=> {
+  expect(user.loggedIn).toEqual(false);
+});
+
+test('User logged in state should not be modified outside of class',  ()=> {
+  expect(()=> {
+    user.loggedIn = true;
+  }).toThrow();
+});
+
+test('User should no email at the beginning',  ()=> {
+  expect(user.email).toBeNull();
+});
+
+test('User email should not be modified outside of class',  ()=> {
+  expect(()=> {
+    user.email = '...';
+  }).toThrow();
+});
