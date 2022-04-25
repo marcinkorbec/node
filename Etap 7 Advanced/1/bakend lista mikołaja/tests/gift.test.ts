62e21885f267580411880dd6de7ae0110d7b37db
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-04-25 17:54:33 +0200

    E7T1D5

--- Etap 7 Advanced/1/bakend lista mikołaja/tests/gift.test.ts
+++ Etap 7 Advanced/1/bakend lista mikołaja/tests/gift.test.ts
@@ -1,18 +1,30 @@
 import {GiftRecord} from "../records/gift.record";
 import {pool} from "../utils/db";
 
-afterAll(async ()=> {
-  await pool.end();
-})
+let gift: GiftRecord;
 
-test('Inserted GiftRecord should have an id', async ()=> {
-  const gift = new GiftRecord({
+beforeAll( ()=> {
+  // 1. Wyczyść całą testowaną tabele bazy danych.
+  // 2. Zwróć przewidywalne dane (po prostu zdefiniowane na stałe).
+  gift = new GiftRecord({
     name: 'Tester',
     count: 1,
   });
   
-  await gift.insert()
+})
+
+afterAll(async ()=> {
+  await pool.end();
+})
+
+test('Not inserted GiftRecord should have no id',  ()=> {
+  console.log(gift.id)
+  expect(gift.id).toBeUndefined();
+})
+
+test('Inserted GiftRecord should have an id', async ()=> {
   
+  await gift.insert();
   expect(gift.id).toBeDefined();
   expect(gift.id).toMatch(/^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/);
 })
--- Etap 7 Advanced/1/bakend lista mikołaja/tests/test.test.ts
+++ Etap 7 Advanced/1/bakend lista mikołaja/tests/test.test.ts
@@ -1,7 +1,42 @@
 import {ChildRecord} from "../records/child.record";
 
-test('foo bar test test', async () => {
-  // const children = await ChildRecord.listAll();
-  // console.log(children)
-  // expect(children).toBeDefined()
+// const myMock = jest
+//   .spyOn(ChildRecord, 'getOne')
+//   .mockImplementation(async (id: string)=> {
+//     return new ChildRecord( {
+//       id,
+//       name: 'Testowy',
+//       giftId: '123',
+//     });
+//   });
+
+let child: ChildRecord;
+
+beforeAll(async ()=> {
+  child = await ChildRecord.getOne('77777777');
+})
+
+jest
+  .spyOn(ChildRecord, 'getOne')
+  .mockImplementation(async (id: string)=> {
+    return new ChildRecord({
+      id,
+      name: "Marcin",
+      giftId: '123',
+    })
+  })
+
+jest
+  .spyOn(ChildRecord.prototype, 'update')
+  .mockImplementation(async ()=> {});
+
+test('foobar test test', async ()=> {
+  const child = await ChildRecord.getOne('88888888');
+  await child.update();
+})
+
+test('foobar test test', async ()=> {
+  const child = ChildRecord.getOne('123');
+  console.log(child)
+  expect(child).toBeDefined();
 })
