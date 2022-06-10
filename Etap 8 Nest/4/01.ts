a7da68c113793d48107e68ae8b250879be9f68bf
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-06-10 21:31:19 +0200

    E8T1D4

---
+++ Etap 8 Nest/4/01.ts
@@ -0,0 +1,11 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function getHello(name, surname) {
+  return `Hello! ${name}, ${surname}`;
+}
+
+function  formatPrice(price, currency) {
+  return price.toFixed(2) + currency;
+}
