7e75c2dd9aa0ee73629d144d8f2e53fad2de853b
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-06-18 00:41:38 +0200

    E8T2D4

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ShopController } from './shop/shop.controller';
-import { ShopService } from './shop/shop.service';
-import { BasketController } from './basket/basket.controller';
-import { BasketService } from './basket/basket.service';
+import { ShopModule } from "./shop/shop.module";
+import { BasketModule } from "./basket/basket.module";
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ShopController, BasketController],
-  providers: [AppService, ShopService, BasketService],
+  imports: [ShopModule, BasketModule, UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
