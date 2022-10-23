50ce61e7c305a5c3e956f5c5b6af3151fdb09622
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-10-23 13:18:06 +0200

    update

--- Etap 6 React/marcin-grygierek/hooki/src/App.tsx
+++ Etap 6 React/marcin-grygierek/hooki/src/App.tsx
@@ -1,6 +1,7 @@
 import React, {useCallback} from 'react';
 import './App.css';
 import {useEffect, useState} from "react";
+import { AppRerender } from './AppRerender';
 
 interface Props {
   handler: ()=> void;
@@ -14,28 +15,28 @@ const Foo = ({handler}: Props) => {
 }
 
 function App() {
-  const [counter, setCounter] = useState<number>(0);
-  const [randomValue, setRandomValue] = useState<number>(0)
+  // const [counter, setCounter] = useState<number>(0);
+  // const [randomValue, setRandomValue] = useState<number>(0)
+  //
+  // useEffect(()=> {
+  //   const interval = setInterval(()=> {
+  //     setCounter(prevCounter => prevCounter + 1);
+  //   }, 1000)
+  //
+  //   const interval2 = setInterval(()=> {
+  //     setRandomValue(Math.random());
+  //   }, 500)
+  //
+  //   return()=> {
+  //     clearInterval(interval)
+  //   }
+  // }, []);
+  //
+  // const handler = useCallback(()=> {
+  //   console.log(counter)
+  // }, [])
 
-  useEffect(()=> {
-    const interval = setInterval(()=> {
-      setCounter(prevCounter => prevCounter + 1);
-    }, 1000)
-
-    const interval2 = setInterval(()=> {
-      setRandomValue(Math.random());
-    }, 500)
-
-    return()=> {
-      clearInterval(interval)
-    }
-  }, []);
-
-  const handler = useCallback(()=> {
-    console.log(counter)
-  }, [])
-
-  return <Foo handler={handler}/>
+  return <AppRerender/>
 }
 
 export default App;
