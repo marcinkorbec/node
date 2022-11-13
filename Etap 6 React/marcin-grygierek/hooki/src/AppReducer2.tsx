f4f330f038278159908bbc09d3bc707cb32fba86
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-11-13 21:48:37 +0100

    update

--- Etap 6 React/marcin-grygierek/hooki/src/AppReducer2.tsx
+++ Etap 6 React/marcin-grygierek/hooki/src/AppReducer2.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useReducer, useState} from "react";
 
 interface User {
   name: string;
@@ -6,19 +6,62 @@ interface User {
   age: number;
 }
 
+interface ChangeNameAction {
+  type: 'CHANGE_NAME';
+  payload: string;
+}
+
+interface ChangeSurnameAction {
+  type: 'CHANGE_SURNAME';
+  payload: string;
+}
+
+interface ChangeAgeAction {
+  type: 'CHANGE_AGE';
+  payload: number;
+}
+
+type UserAction = ChangeSurnameAction | ChangeNameAction | ChangeAgeAction;
+
+const initialState: User = {
+  name:'Marcin',
+  surname: 'Korbecki',
+  age: 34,
+}
+
+const userReducer = (state:User, action:UserAction) => {
+  switch(action.type) {
+    case 'CHANGE_NAME': {
+      return {
+        ...state,
+        name: action.payload
+      }
+    }
+    case 'CHANGE_SURNAME': {
+      return {
+        ...state,
+        surname: action.payload
+      }
+    }
+    case 'CHANGE_AGE': {
+      return {
+        ...state,
+        age: state.age + action.payload,
+      }
+    }
+    default:
+      return state;
+  }
+}
 export const AppReducer2 = () => {
-  const [user, setUser]= useState<User>({
-    name:'Marcin',
-    surname: 'Korbecki',
-    age: 34,
-  })
+  const [user, dispatch]= useReducer(userReducer, initialState);
 
 
 
   return <div>
     <h1>{user.name} {user.surname} ma {user.age} lat.</h1>
-    <button >Zmień imię</button>
-    <button>Zmień nazwisko</button>
-    <button>Postarz</button>
+    <button onClick={() => dispatch({type: "CHANGE_NAME", payload: 'Maria'})}>Zmień imię</button>
+    <button onClick={() => dispatch({type: "CHANGE_SURNAME", payload: 'Korbecka'})}>Zmień nazwisko</button>
+    <button onClick={() => dispatch({type: "CHANGE_AGE", payload: 1})}>Postarz</button>
   </div>
 }
