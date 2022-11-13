d85f204b272e6341de87c2816ce80c556b5e0a0b
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-11-13 18:38:35 +0100

    update

--- Etap 6 React/marcin-grygierek/hooki/src/AppRef.tsx
+++ Etap 6 React/marcin-grygierek/hooki/src/AppRef.tsx
@@ -3,6 +3,7 @@ import {useRef, useState} from "react";
 export const AppRef = () => {
 
   const inputRef = useRef<HTMLInputElement | null>(null);
+  const randomRef = useRef<number | null>(null)
   const [random, setRandom] = useState<number>(0);
 
   const handleClick = () => {
@@ -12,11 +13,13 @@ export const AppRef = () => {
   }
 
   const randomize = () => {
-    setRandom(Math.random() * 100);
+    randomRef.current = random;
+    setRandom(Math.floor(Math.random() * 1000));
   }
 
   return <div>
     <h1>{random}</h1>
+    {randomRef.current && <h2>{Math.abs(randomRef.current - random)}</h2>}
     <input ref={inputRef} type="text"/>
     <button onClick={handleClick}>Kliknij mnie!</button>
     <button onClick={randomize}>Wylosuj!</button>
