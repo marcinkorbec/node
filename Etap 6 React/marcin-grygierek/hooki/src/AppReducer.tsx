96f6b5d9e5e930785bbc5f4504be2cd8c6e4cd32
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-11-13 19:39:32 +0100

    update

--- Etap 6 React/marcin-grygierek/hooki/src/AppReducer.tsx
+++ Etap 6 React/marcin-grygierek/hooki/src/AppReducer.tsx
@@ -13,8 +13,32 @@ export const AppReducer = () => {
     age: 34,
   })
 
+  const changeName = (name: string) => {
+    setUser(prevUser => ({
+      ...prevUser,
+      name
+    }))
+  }
+
+  const changeSurname = (surname: string) => {
+    setUser(prevUser => ({
+      ...prevUser,
+      surname
+    }))
+  }
+
+  const changeAge = () => {
+    setUser(prevUser => ({
+      ...prevUser,
+      age: prevUser.age + 1,
+    }))
+  }
+
+
   return <div>
     <h1>{user.name} {user.surname} ma {user.age} lat.</h1>
-
+    <button onClick={()=> changeName('Maria')}>Zmień imię</button>
+    <button onClick={() => changeSurname('Korbecka')}>Zmień nazwisko</button>
+    <button onClick={() => changeAge()}>Postarz</button>
   </div>
 }
