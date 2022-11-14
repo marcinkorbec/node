21ba212c8092025dc1fac6d9a500324f0c5c0d9b
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-11-14 21:19:41 +0100

    update

--- Etap 6 React/marcin-grygierek/hooki/src/Calculator.tsx
+++ Etap 6 React/marcin-grygierek/hooki/src/Calculator.tsx
@@ -12,43 +12,62 @@ export const Calculator = () => {
   const [first, setFirst] = useState<number>(0)
   const [second, setSecond] = useState<number>(0)
   const [result, setResult] = useState<number>(0)
+  const [storyOperation, setStoryOperation] = useState<string[]>([])
 
   const handleFirstChange = (e: ChangeEvent<HTMLInputElement>) => {
     setFirst(Number(e.target.value));
   }
+
   const handleSecondChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSecond(Number(e.target.value));
+  }
+
+  const addToHistory = (line: string) => {
+    setStoryOperation(prevHistory => [...prevHistory, line]);
   }
 
   const handleOperation = (operation:Operation) => {
     switch (operation) {
       case Operation.ADD: {
         setResult(first + second)
+        addToHistory(`${first} + ${second} = ${first + second}`)
+        break;
       }
       case Operation.SUBTRACT: {
         setResult(first - second)
+        addToHistory(`${first} - ${second} = ${first - second}`)
+        break;
       }
       case Operation.MULTIPLY: {
         setResult(first * second)
+        addToHistory(`${first} * ${second} = ${first * second}`)
+        break;
       }
       case Operation.DIVIDE: {
-        setResult(first / second)
+        if (second === 0) {
+          alert('Próbujesz podzielić przez zero!')
+        }
+        else {
+          setResult(first / second);
+          addToHistory(`${first} / ${second} = ${first / second}`)
+        }
+        break;
       }
     }
   }
 
   return <div>
-    <input type="number" value={first}/>
-    <input type="number" value={second}/>
+    <input type="number" value={first} onChange={handleFirstChange}/>
+    <input type="number" value={second} onChange={handleSecondChange}/>
 
     <h1>{result}</h1>
-    <button>+</button>
-    <button>-</button>
-    <button>*</button>
-    <button>/</button>
+    <button onClick={() => handleOperation(Operation.ADD)}>+</button>
+    <button onClick={() => handleOperation(Operation.SUBTRACT)}>-</button>
+    <button onClick={() => handleOperation(Operation.MULTIPLY)}>*</button>
+    <button onClick={() => handleOperation(Operation.DIVIDE)}>/</button>
 
     <ul>
-      <li></li>
+      {storyOperation.map((line, index)=> <li key={index}>{line}</li>)}
     </ul>
   </div>
-}
+};
