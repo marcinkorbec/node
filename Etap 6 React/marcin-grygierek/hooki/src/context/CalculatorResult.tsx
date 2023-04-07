301fea5f8ee77cd67bd66daf1e258e872780c7af
Marcin Korbecki <marcin.korbecki@mayeryn.com>
2023-04-07 15:44:02 +0200

    add context

--- Etap 6 React/marcin-grygierek/hooki/src/context/CalculatorResult.tsx
+++ Etap 6 React/marcin-grygierek/hooki/src/context/CalculatorResult.tsx
@@ -1,5 +1,10 @@
+import { useContext } from "react"
+import { CalculatorContext } from "./Calculator";
 
 export const CalculatorResult = () => {
+  const context = useContext(CalculatorContext);
+  if (!context) return null;
+  const {result, first, second} = context;
   return (
     <>
       <h1>{result}</h1>
