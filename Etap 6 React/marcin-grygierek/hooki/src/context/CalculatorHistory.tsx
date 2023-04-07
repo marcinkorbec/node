0d372b1a107486c52b0b16a3070556a2028341cc
Marcin Korbecki <marcin.korbecki@mayeryn.com>
2023-04-07 15:43:34 +0200

    add context

--- Etap 6 React/marcin-grygierek/hooki/src/context/CalculatorHistory.tsx
+++ Etap 6 React/marcin-grygierek/hooki/src/context/CalculatorHistory.tsx
@@ -1,9 +1,15 @@
+import { CalculatorContext } from "./Calculator";
+import { useContext } from "react"
 
 
 export const CalculatorHistory = () => {
+  const context = useContext(CalculatorContext);
+  if (!context) return null;
+
+  const { history } = context;
   return (
     <ul>
-      {storyOperation.map((line, index)=> <li key={index}>{line}</li>)}
+      {history.map((line, index) => <li key={index}>{line}</li>)}
     </ul>
   )
 }
