6e6b7374be562dd86f9124dd3d46b610ca5f8032
Marcin Korbecki <marcin.korbecki@mayeryn.com>
2023-04-28 23:44:46 +0200

    Add redux

---
+++ Etap 6 React/marcin-grygierek/hooki/src/redux/actions/calculator.ts
@@ -0,0 +1,25 @@
+import {CalculatorAction} from "../action-types/calculator";
+
+export const setFirst = (first: number) => ({
+  type: CalculatorAction.SET_FIRST,
+  payload: first
+})
+
+export const setSecond = (second: number) => ({
+  type: CalculatorAction.SET_SECOND,
+  payload: second
+})
+
+export const setResult = (result: number | string) => ({
+  type: CalculatorAction.SET_RESULT,
+  payload: result
+})
+
+export const AddToHistory = (line: string[]) => ({
+  type: CalculatorAction.ADD_TO_HISTORY,
+  payload: line
+})
+
+export const ClearHistory = () => ({
+  type: CalculatorAction.SET_FIRST,
+})
