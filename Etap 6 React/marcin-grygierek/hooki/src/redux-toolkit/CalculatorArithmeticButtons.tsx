3ada9baa38cdab250b4ec42aaf8d42592c563a2a
Marcin Korbecki <marcin.korbecki@mayeryn.com>
2023-05-03 22:30:55 +0200

    redux-toolkit

---
+++ Etap 6 React/marcin-grygierek/hooki/src/redux-toolkit/CalculatorArithmeticButtons.tsx
@@ -0,0 +1,55 @@
+import {useDispatch, useSelector} from "react-redux";
+import {RootState} from "./store";
+import {addToHistory, setResult} from "./features/calculator/calculator-slice";
+import {Operation} from "./Calculator";
+
+export const CalculatorArithmeticButtons = () => {
+  const dispatch = useDispatch();
+  const {first, second} = useSelector((store: RootState) => store.calculator);
+
+  const handleSetOperationResult = (result: string | number) => {
+    dispatch(setResult(result));
+  }
+
+  const handleAddToHistory = (line: string) => {
+    dispatch(addToHistory(line));
+  }
+
+  const handleOperation = (operation:Operation) => {
+    switch (operation) {
+      case Operation.ADD: {
+        handleSetOperationResult(first + second)
+        handleAddToHistory(`${first} + ${second} = ${first + second}`)
+        break;
+      }
+      case Operation.SUBTRACT: {
+        handleSetOperationResult(first - second)
+        handleAddToHistory(`${first} - ${second} = ${first - second}`)
+        break;
+      }
+      case Operation.MULTIPLY: {
+        handleSetOperationResult(first * second)
+        handleAddToHistory(`${first} * ${second} = ${first * second}`)
+        break;
+      }
+      case Operation.DIVIDE: {
+        if (second === 0) {
+          alert('Próbujesz podzielić przez zero!')
+        }
+        else {
+          handleSetOperationResult(first / second);
+          handleAddToHistory(`${first} / ${second} = ${first / second}`)
+        }
+        break;
+      }
+    }
+  }
+  return (
+    <>
+      <button onClick={() => handleOperation(Operation.ADD)}>+</button>
+      <button onClick={() => handleOperation(Operation.SUBTRACT)}>-</button>
+      <button onClick={() => handleOperation(Operation.MULTIPLY)}>*</button>
+      <button onClick={() => handleOperation(Operation.DIVIDE)}>/</button>
+    </>
+  )
+}
